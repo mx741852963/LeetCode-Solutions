63441b027f0c22bbf1ebff4a838ab775af08ffdc
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-15 18:02:19 +0300

    1768-merge-strings-alternately Easy Two Pointers 31 ms (99.33%) 19.4 MB (17.74%)

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,28 +1,9 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        word_one_len = len(word1)
-        word_two_len = len(word2)
-        word_flag = 1  # word1
         merged = []
-        i = 0
-        j = 0
-        while i < word_one_len and j < word_two_len:
-            if word_flag:
-                merged.append(word1[i])
-                i += 1
-                word_flag = 0
-            else:
-                merged.append(word2[j])
-                j += 1
-                word_flag = 1
-        if i < word_one_len:
-            merged.append(word1[i:])
-            i += 1
-        if j < word_two_len:
-            merged.append(word2[j:])
-            j += 1
-
-        return ''.join(merged)  # Time : O(word1+word2) , Space : O(word1+word2)
-
-        return ''.join(merged) # Time : O(word1+word2) , Space : O(word1+word2) 
+        for char1, char2 in zip(word1, word2):
+            merged.append(char1)
+            merged.append(char2)
+        min_len = min(len(word1), len(word2))
+        return "".join(merged) + word1[min_len:] + word2[min_len:]
             
