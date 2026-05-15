3b3c6df60a76e2089a86e5b804eb2069e93544d6
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-15 10:42:06 +0300

    0643-maximum-average-subarray-i Easy Array 84 ms (25.07%) 29.2 MB (53.2%)

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,11 +1,15 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        current_sum = sum(nums[:k])
-        max_sum = current_sum
-        for i in range(k, len(nums)):
-            current_sum += nums[i] - nums[i - k]
-            if current_sum > max_sum:
-                max_sum = current_sum
-        return max_sum / k
+        left = 0
+        n = len(nums)
+        for i in range(k):
+            left += nums[i]
+        max_avg = left / k
+        for i in range(k, n):
+            left += nums[i]
+            left -= nums[i - k]
+            avg = left / k
+            max_avg = max(max_avg, avg)
+        return max_avg
 # Time : O(n)
-# Space : O(n)
+# Space : O(1)
