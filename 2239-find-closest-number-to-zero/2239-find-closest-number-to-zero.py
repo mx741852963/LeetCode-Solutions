0d6a262fe206fb5f768e4c3dc8875c4ce2e80c7f
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-15 16:02:17 +0300

    2239-find-closest-number-to-zero Easy Array 7 ms (45.18%) 19.4 MB (52.59%)

---
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        closest=nums[0]
+        for i in nums:
+            if abs(i)<abs(closest):
+                closest = i 
+        if closest < 0 and abs(closest) in nums:
+            return abs(closest)
+        else:
+            return closest        
+# Time O(n)
+# Space O(1)
