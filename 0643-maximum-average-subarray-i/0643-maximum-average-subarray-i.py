ad6ccff309828c32c7aa9fe4809f8b62e5282e92
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-15 10:56:55 +0300

    0643-maximum-average-subarray-i Easy Array 42 ms (96.98%) 29.4 MB (18.55%)

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,8 +1,11 @@
+import sys
+input = sys.stdin.read
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         cur_sum = sum(nums[:k])
         max_sum = cur_sum
-        for i in range(k, len(nums)):
+        lenn = len(nums)
+        for i in range(k, lenn):
             cur_sum += nums[i] - nums[i - k] 
             if cur_sum > max_sum:
                 max_sum = cur_sum
@@ -10,4 +13,4 @@ class Solution:
 
 
 # Time : O(n)
-# Space : O(n)
+# Space : O(1)
