652c6481d2d72b24985e590f3bc58903977f93a1
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-15 15:45:47 +0300

    0509-fibonacci-number Easy Math 51 ms (46.14%) 19.2 MB (19.13%)

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,12 +1,44 @@
 class Solution:
     def fib(self, n: int) -> int:
-        cache = {0:0,1:1}
-        def f(num):
-            if num in cache:
-                return cache[num]
-            else:
-                cache[num] = f(num-1)+f(num-2)
-                return cache[num]
-        return f(n)
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+        prev= 0
+        cur = 1 
+        for i in range(2,n+1):
+            prev,cur = cur,cur+prev
+        return cur
+# Time O(n)
+# Space O(1)
+
+
+# class Solution:
+#     def fib(self, n: int) -> int:
+#         if n==0:
+#             return 0
+#         if n==1:
+#             return 1
+#         dp = [0]*(n+1)
+#         dp[0]=0
+#         dp[1]=1
+#         for i in range(2,n+1):
+#             dp[i] = dp[i-2]+dp[i-1]
+#         return dp[-1]
+# Time O(n)
+# Space O(n)
+
+
+
+# class Solution:
+#     def fib(self, n: int) -> int:
+#         cache = {0:0,1:1}
+#         def f(num):
+#             if num in cache:
+#                 return cache[num]
+#             else:
+#                 cache[num] = f(num-1)+f(num-2)
+#                 return cache[num]
+#         return f(n)
 # Time O(n)
 # Space O(n)
