d1bbcf32bac5e1626bced8a6543dc181c0374ce7
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-15 20:54:14 +0300

    0013-roman-to-integer Easy Hash Table 7 ms (42.84%) 19.3 MB (19.99%)

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dictt = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        lenn = len(s)
+        summ = 0
+        i = 0
+        while i < lenn:
+            if i < lenn - 1 and dictt[s[i]] < dictt[s[i + 1]]:
+                summ += dictt[s[i + 1]] - dictt[s[i]]
+                i += 2
+            else:
+                summ += dictt[s[i]]
+                i += 1
+        return summ
+                          
