728c97b41120307ca5a4fbdae977e00bae008b4d
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-15 14:56:55 +0300

    0078-subsets Medium Array 0 ms (100%) 19.5 MB (28.17%)

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        lenn = len(nums)
+        res, sol = [], []
+
+        def backtrack(i):
+            if i == lenn:
+                res.append(sol[:])
+                return
+            backtrack(i + 1)
+            sol.append(nums[i])
+            backtrack(i + 1)
+            sol.pop()
+
+        backtrack(0)
+        return res
+# Time O(2^n)
+# Space O(n) 
