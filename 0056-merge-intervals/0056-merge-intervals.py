d8d5f3b360398f9211a455ee136a653d02f6558f
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-16 14:20:32 +0300

    0056-merge-intervals Medium Array 7 ms (71.98%) 22.6 MB (58.25%)

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        sorted_I = sorted(intervals, key=lambda t: t[0])
+        ans= []
+        for interval in sorted_I:
+            if not ans or ans[-1][1]<interval[0]:
+                ans.append(interval)
+            else:
+                ans[-1] = [ans[-1][0], max(ans[-1][1], interval[1])]
+        return ans
+# Time O(n log n )
+# Space O(n)
