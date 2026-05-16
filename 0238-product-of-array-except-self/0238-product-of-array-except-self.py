15b1ee36278a1b1638a85a5f387ec71b79e4ae5d
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-16 13:25:21 +0300

    0238-product-of-array-except-self Medium Array 29 ms (36.85%) 26.6 MB (36.03%)

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,24 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-            r = [1]
-            l = [1]
-            ans=[]
-            i = 0
-            j = 0
-            while i < len(nums) - 1:
-                l.append(l[j] * nums[i])
-                i += 1
-                j = i
-            nums.reverse()
-            i = 0
-            j = 0
-            while i < len(nums) - 1:
-                r.append(r[j] * nums[i])
-                i += 1
-                j = i
-            r.reverse()
-            x=0
-            while x < len(nums):
-                ans.append(r[x]*l[x])
-                x += 1
-            return ans
+        l = 1
+        r = 1
+        n = len(nums)
+        l_arr, r_arr = [0] * n, [0] * n
+        for i in range(n):
+            j = -i - 1
+            l_arr[i] = l
+            r_arr[j] = r
+            r *= nums[j]
+            l *= nums[i]
+        return [l * r for l, r in zip(l_arr, r_arr)]
