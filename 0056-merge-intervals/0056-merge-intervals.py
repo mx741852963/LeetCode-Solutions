72e3027f8308cf726a1822c9170d1ad87b138f9c
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-16 14:27:02 +0300

    0056-merge-intervals Medium Array 0 ms (100%) 22.6 MB (58.25%)

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -6,7 +6,7 @@ class Solution:
             if not ans or ans[-1][1]<interval[0]:
                 ans.append(interval)
             else:
-                ans[-1] = [ans[-1][0], max(ans[-1][1], interval[1])]
+                ans[-1][1] = max(ans[-1][1], interval[1])
         return ans
 # Time O(n log n )
 # Space O(n)
