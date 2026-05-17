c28bc7e525aac1ecb4f337792eaa55a77bca4332
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-17 12:46:36 +0300

    0242-valid-anagram Easy Hash Table 17 ms (19.76%) 19.8 MB (20.75%)

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,8 +2,9 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         sorted_string_t = "".join(sorted(t))
         sorted_string_s = "".join(sorted(s))
+        if len(s)!=len(t):
+            return False
         if sorted_string_t == sorted_string_s :
             return True 
         else:
             return False
-        
