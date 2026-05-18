16f2836fcf0a527104338c488e97ffca028fd69d
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-18 08:22:06 +0300

    0001-two-sum Easy Array 0 ms (100%) 20.4 MB (42.88%)

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         h = {}
-        for  i in range (len(nums)):
-            h[nums[i]] = i
-        for i in range (len(nums)):
-            y = target - nums[i]
-            if y in h and h[y] != i:
-                return [i,h[y]]
+        for i, num in enumerate(nums):
+            y = target - num
+            if y in h:
+                return [h[y], i]
+            h[num] = i
+        # for  i in range (len(nums)):
+        #     h[nums[i]] = i
+        # for i in range (len(nums)):
+        #     y = target - nums[i]
+        #     if y in h and h[y] != i:
+        #         return [i,h[y]]
+# Time = O(n)
+# Space = O(n)
