ebcd7fd4e79656d37dea4461f84d094748c3ee97
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-18 19:46:24 +0300

    0062-unique-paths Medium Math 0 ms (100%) 20 MB (5.15%)

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,4 +1,32 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        import math
-        return math.comb((m-1)+(n-1),n-1)
+        # Combinations Solution
+        # import math
+        # return math.comb((m-1)+(n-1),n-1)
+        # Recursive Solution
+        # Time O(2^(n*m))
+        # Space = O(n*m)
+        # def paths(i,j):
+        #     if i == j== 0:
+        #         return 1
+        #     elif i < 0 or j < 0 or i == m or j == n:
+        #         return 0 
+        #     else:
+        #         return paths(i,j-1) + paths(i-1,j)
+        # return paths(m-1,n-1)
+        # Top Down Solution DP (Memoization)
+        # Time O(n*m)
+        # Space = O(n*m)
+        memo = {(0,0):1}
+        def paths(i,j):
+            if (i,j) in memo:
+                return memo[(i,j)]
+            elif i < 0 or j < 0 or i == m or j == n:
+                return 0 
+            else:
+                val=  paths(i,j-1) + paths(i-1,j)
+                memo[(i,j)]  = val
+                return val
+        return paths(m-1,n-1)
+
+
