94061faf2e16e8635aa68d9ad8f4c122607f4ce0
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-19 08:56:18 +0300

    0049-group-anagrams Medium Array 174 ms (5.35%) 24.2 MB (9.91%)

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -6,6 +6,9 @@ class Solution:
             for char in str_:
                 listt[ord(char) - 97] += 1
             tuplee = tuple(listt)
-            counter[tuplee] = counter.get(tuplee, []) + [str_] 
+            if tuplee not in counter:
+                counter[tuplee] = []
+            counter[tuplee].append(str_)
+            # counter[tuplee] = counter.get(tuplee, []) + [str_] 
             val = list(counter.values())
         return val
