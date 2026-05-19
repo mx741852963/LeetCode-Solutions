7e59aa5b45c764c014d50ac9959c3a4ff73d6cba
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-19 08:54:07 +0300

    0049-group-anagrams Medium Array 177 ms (5.35%) 23.6 MB (33.08%)

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        counter={}
+        for str_ in strs:
+            listt = [0] * 26
+            for char in str_:
+                listt[ord(char) - 97] += 1
+            tuplee = tuple(listt)
+            counter[tuplee] = counter.get(tuplee, []) + [str_] 
+            val = list(counter.values())
+        return val
