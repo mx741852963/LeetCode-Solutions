ad01b629c0c38437ea83c81433a4eaf7948ddcf9
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-19 19:27:55 +0300

    1189-maximum-number-of-balloons Easy Hash Table 0 ms (100%) 19.4 MB (39.1%)

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,24 +1,11 @@
-from collections import defaultdict
+from collections import Counter
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        main_text = "balloon"
-        hashmap = defaultdict(int)
-        counter = 0
-        i = 0
-        lenn = len(main_text)
-        for txt in text:
-            hashmap[txt] += 1
-        while True:
-            if i == lenn :
-                i = 0
-            char = main_text[i]
-            if char == 'n' and hashmap[char] != 0:
-                counter += 1
-            if hashmap[char] == 0:
-                del hashmap[char]
-            if char in hashmap:
-                hashmap[char] -= 1
-                i += 1
-            else:
-                break
-        return counter
+        counts = Counter(text)
+        return min(
+            counts['b'], 
+            counts['a'], 
+            counts['l'] // 2, 
+            counts['o'] // 2, 
+            counts['n']
+        )
