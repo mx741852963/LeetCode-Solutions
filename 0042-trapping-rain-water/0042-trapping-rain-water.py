67aa858f2586049a3e14d3597629f7ab7bd2b373
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-20 12:18:45 +0300

    0042-trapping-rain-water Hard Array 4 ms (84.5%) 21 MB (83.8%)

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,18 +1,34 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left_wall = right_wall = 0
-        lenn = len(height)
-        max_left = [0]*lenn
-        max_right = [0]*lenn
-        summ =  0
-        for i in range(lenn):
-            j = -i -1
-            max_left[i],max_right[j]=left_wall,right_wall
-            left_wall,right_wall = max(left_wall,height[i]),max(right_wall,height[j])
-        for i in range(lenn):
-            pot = min(max_left[i],max_right[i])
-            summ += max(0,pot-height[i])
-        return summ 
+        count = 0
+        left_side = 0
+        right_side = len(height) - 1
+        max_left_side = height[left_side]
+        max_right_side = height[right_side]
+        while left_side < right_side:
+            if max_left_side < max_right_side:
+                left_side += 1
+                max_left_side = max(max_left_side, height[left_side])
+                count += max_left_side - height[left_side]
+            else:
+                right_side -= 1
+                max_right_side = max(max_right_side, height[right_side])
+                count += max_right_side - height[right_side]
+        return count
+
+        # left_wall = right_wall = 0
+        # lenn = len(height)
+        # max_left = [0]*lenn
+        # max_right = [0]*lenn
+        # summ =  0
+        # for i in range(lenn):
+        #     j = -i -1
+        #     max_left[i],max_right[j]=left_wall,right_wall
+        #     left_wall,right_wall = max(left_wall,height[i]),max(right_wall,height[j])
+        # for i in range(lenn):
+        #     pot = min(max_left[i],max_right[i])
+        #     summ += max(0,pot-height[i])
+        # return summ 
         # Time O(n)
         # Space O(n)
         # maxx = max(height)
