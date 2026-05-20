7b5518bdda01c1615ef95a3d5f7591126d44133a
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-20 08:48:17 +0300

    0344-reverse-string Easy Two Pointers 39 ms (11.08%) 23.3 MB (80.59%)

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,11 +3,13 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        i = 0
-        j = len(s) - 1
-        k = 0
-        while k < len(s) // 2:
-            s[i], s[j] = s[j], s[i]
-            i += 1
-            j -= 1
-            k += 1
+        for i in range(len(s)//2):
+            s[i], s[-1-i] = s[-1-i], s[i]
+        # i = 0
+        # j = len(s) - 1
+        # k = 0
+        # while k < len(s) // 2:
+        #     s[i], s[j] = s[j], s[i]
+        #     i += 1
+        #     j -= 1
+        #     k += 1
