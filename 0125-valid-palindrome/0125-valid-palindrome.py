320524b22004afcf1d7e9fb2fd6aa2aedbc36a44
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-20 13:16:50 +0300

    0125-valid-palindrome Easy Two Pointers 8 ms (48.99%) 23.2 MB (17.15%)

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,8 +5,6 @@ class Solution:
         valid = "".join([char.lower() for char in s if char.isalnum()])
         lenn= len(valid) // 2
         for i in range(lenn):
-            if not valid or len(valid) ==1:
-                return True
             j = -i - 1
             if valid[i] != valid[j]:
                 return False
