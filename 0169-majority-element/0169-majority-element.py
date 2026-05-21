42f78632cc114de6472a0c6bd844ca26dc21c2d4
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-21 13:57:31 +0300

    0169-majority-element Easy Array 0 ms (100%) 21.4 MB (20.36%)

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,9 +1,23 @@
 from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        return count.most_common(1)[0][0]
+        # count = Counter(nums)
+        # return count.most_common(1)[0][0]
         # maxx = max(count.values())
         # inverted_dict = {v: k for k, v in count.items()}
         # target_key = inverted_dict.get(maxx)  
         # return target_key
+        # Time O(n)
+        # Space o(n)
+        ans = -1
+        count = 0
+        for num in nums :
+            if count == 0:
+                ans = num
+            if ans ==num:
+                count +=1
+            else :
+                count -=1
+        return ans
+# Time O(n)
+# Space O(1)
