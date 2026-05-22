b2b419af2e1af90b96445121bf29ae75431b0d2c
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-22 13:10:28 +0300

    0062-unique-paths Medium Math 0 ms (100%) 19.3 MB (42.02%)

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -17,34 +17,34 @@ class Solution:
         # Top Down Solution DP (Memoization)
         # Time O(n*m)
         # Space = O(n*m)
-        memo = {(0,0):1}
-        def paths(i,j):
-            if (i,j) in memo:
-                return memo[(i,j)]
-            elif i < 0 or j < 0 or i == m or j == n:
-                return 0 
-            else:
-                val=  paths(i,j-1) + paths(i-1,j)
-                memo[(i,j)]  = val
-                return val
-        return paths(m-1,n-1)
+        # memo = {(0,0):1}
+        # def paths(i,j):
+        #     if (i,j) in memo:
+        #         return memo[(i,j)]
+        #     elif i < 0 or j < 0 or i == m or j == n:
+        #         return 0 
+        #     else:
+        #         val=  paths(i,j-1) + paths(i-1,j)
+        #         memo[(i,j)]  = val
+        #         return val
+        # return paths(m-1,n-1)
         # Bottom Up Solution DP (Tabulation)
         # Time O(n*m)
         # Space = O(n*m)
-        # dp=[]
-        # for _ in range(m):
-        #     dp.append([0]*n)
-        # dp[0][0] = 1 
-        # for i in range(m):
-        #     for j in range(n):
-        #         if j == i ==0:
-        #             continue
-        #         val = 0
-        #         if i > 0 :
-        #             val+=dp[i-1][j]
-        #         if j > 0 :
-        #             val+=dp[i][j-1]
-        #         dp[i][j]=val
-        # return dp[m-1][n-1]
+        dp=[]
+        for _ in range(m):
+            dp.append([0]*n)
+        dp[0][0] = 1 
+        for i in range(m):
+            for j in range(n):
+                if j == i ==0:
+                    continue
+                val = 0
+                if i > 0 :
+                    val+=dp[i-1][j]
+                if j > 0 :
+                    val+=dp[i][j-1]
+                dp[i][j]=val
+        return dp[m-1][n-1]
 
 
