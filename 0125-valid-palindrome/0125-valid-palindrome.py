1cfefac9a79bdc0d928f02b40ea78c55f7aaeccd
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-22 17:49:28 +0300

    0125-valid-palindrome Easy Two Pointers 11 ms (33.52%) 24.2 MB (5.85%)

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        valid = ""
-        lenn = len(s)
-        valid = "".join([char.lower() for char in s if char.isalnum()])
-        lenn= len(valid) // 2
-        for i in range(lenn):
-            j = -i - 1
-            if valid[i] != valid[j]:
-                return False
-        return True
+        valid =[char.lower() for char in s if char.isalnum()]
+        return valid == valid[::-1]
+    #     valid =[char.lower() for char in s if char.isalnum()]
+    #     lenn= len(valid) // 2
+    #     for i in range(lenn):
+    #         if valid[i] != valid[-i - 1]:
+    #             return False
+    #     return True
+    # # Time = O(n)
+    # # Space = O(n)
