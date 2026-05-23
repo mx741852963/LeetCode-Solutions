ae20efbe337e5a87a08410c26fd6f074fad66f1f
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-23 12:29:43 +0300

    0682-baseball-game Easy Array 0 ms (100%) 19.5 MB (43.84%)

---
+++ 0682-baseball-game/0682-baseball-game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        res = []
+        for num in operations:
+            match num :
+                case '+' :
+                    res.append(res[-1]+res[-2])
+                case 'D':
+                    res.append(res[-1] * 2)
+                case 'C':
+                    res.pop()
+                case _ :
+                    res.append(int(num))
+        return sum(res)
