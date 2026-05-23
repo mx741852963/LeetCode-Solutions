fac367bc294a255b65275e842aadd61dd9a3ad23
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-23 15:39:15 +0300

    0020-valid-parentheses Easy String 3 ms (34.45%) 19.4 MB (27.47%)

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,40 @@
+from collections import Counter
+class Solution:
+    def isValid(self, s: str) -> bool:
+        count = Counter(s)
+        ss = ')}]'
+        lenn= len(s)
+        res = []
+        if (lenn & 1) == 0 :
+            if count['('] == count[')'] and count['['] == count[']'] and count['{'] == count['}']  :
+                for i in range(lenn):
+                    if s[0]  in ss:
+                        break
+                    if s[-1] not in ss:
+                        break
+                    match s[i] :
+                        case '('|'{'|'[' :
+                            res.append(s[i])
+                        case _  :
+                            chek = res.pop()
+                            match chek:
+                                case '(':
+                                    if s[i] == ']' or s[i] == '}' :
+                                        break
+                                    else:
+                                        continue
+                                case '{':
+                                    if s[i] == ']' or s[i] == ')' :
+                                        break
+                                    else:
+                                        continue
+                                case '[':
+                                    if s[i] == ')' or s[i] == '}' :
+                                        break
+                                    else:
+                                        continue
+                if not res and i == lenn - 1 :
+                        return True
+
+                    
+        return False
