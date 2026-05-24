8d6932dd57da4f57f7e4a0807bd36c4236c6c1e8
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-24 14:12:48 +0300

    0074-search-a-2d-matrix Medium Array 0 ms (100%) 19.6 MB (40.3%)

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,21 +1,50 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m = len(matrix)
-        n = len(matrix[0])
-        t = m * n
-        l = 0
-        r = t - 1
+        if not matrix or not matrix[0]:
+            return False
+        rows, cols = len(matrix), len(matrix[0])
+        top, bot = 0, rows - 1
+        target_row = -1
+        while top <= bot:
+            mid_row = (top + bot) // 2
+            if target < matrix[mid_row][0]:
+                bot = mid_row - 1
+            elif target > matrix[mid_row][-1]:
+                top = mid_row + 1
+            else:
+                target_row = mid_row
+                break
+        if target_row == -1:
+            return False
+        row = matrix[target_row]
+        l, r = 0, cols - 1
         while l <= r:
             mid = (l + r) // 2
-            i = mid // n
-            j = mid % n
-            mid_nums = matrix[i][j]
-            if mid_nums == target:
+            if row[mid] == target:
                 return True
-            elif mid_nums < target:
+            elif row[mid] < target:
                 l = mid + 1
             else:
                 r = mid - 1
         return False
-# Time O(log(m * n))
-# Space O(1)
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         m = len(matrix)
+#         n = len(matrix[0])
+#         t = m * n
+#         l = 0
+#         r = t - 1
+#         while l <= r:
+#             mid = (l + r) // 2
+#             i = mid // n
+#             j = mid % n
+#             mid_nums = matrix[i][j]
+#             if mid_nums == target:
+#                 return True
+#             elif mid_nums < target:
+#                 l = mid + 1
+#             else:
+#                 r = mid - 1
+#         return False
+# # Time O(log(m * n))
+# # Space O(1)
