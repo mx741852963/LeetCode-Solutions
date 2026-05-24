3c7324cb28034eaadf6067f9c17e68d00e0dc442
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-24 19:42:29 +0300

    0153-find-minimum-in-rotated-sorted-array Medium Array 0 ms (100%) 19.3 MB (92.71%)

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -3,15 +3,26 @@ class Solution:
         lenn = len(nums)
         r = lenn - 1
         l = 0 
-        target = float('inf')
         if nums[l] <= nums[r]:
             return nums[l]
-        while l <= r :
-            mid = (l+r)//2
-            if nums[r] < nums[mid]:
-                target = min(nums[mid],target)
-                l = mid + 1
-            else:
-                target = min(nums[mid],target)
-                r = mid - 1
-        return target 
+        while l < r :
+            mid = (r+l)//2
+            if nums[mid] > nums[r]:
+                l  = mid + 1 
+            else :
+                r = mid
+        return nums[l]
+        # Time O(log n )
+        # Space O(1)
+        # target = float('inf')
+        # if nums[l] <= nums[r]:
+        #     return nums[l]
+        # while l <= r :
+        #     mid = (l+r)//2
+        #     if nums[r] < nums[mid]:
+        #         target = min(nums[mid],target)
+        #         l = mid + 1
+        #     else:
+        #         target = min(nums[mid],target)
+        #         r = mid - 1
+        # return target 
