6e7daef51eb12520e68ac7c42ed05316a1f9dfae
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-25 16:01:33 +0300

    0875-koko-eating-bananas Medium Array 24 ms (99.49%) 32.9 MB (12.31%)

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,16 +1,34 @@
+# class Solution:
+#     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+#         def dose_k(k):
+#             hours = 0
+#             for p in piles:
+#                 hours += ceil(p / k)
+#             return hours <= h
+#         if len(piles) == 1 and piles[0]==h :
+#             return piles[0]
+#         if len(piles) == h:
+#             return max(piles)
+#         l = ceil(sum(piles)/h)
+#         r = max(piles)
+#         while l < r:
+#             k = (l + r) // 2
+#             if dose_k(k):
+#                 r = k
+#             else:
+#                 l = k + 1
+#         return r
+import numpy as np
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        p = np.array(piles)
         def dose_k(k):
-            hours = 0
-            for p in piles:
-                hours += ceil(p / k)
-            return hours <= h
+            return np.ceil(p/k).sum() <= h
         if len(piles) == 1 and piles[0]==h :
             return piles[0]
         if len(piles) == h:
             return max(piles)
-        # l = ceil(sum(piles)/h)
-        l = 1
+        l = np.ceil(sum(p/h))
         r = max(piles)
         while l < r:
             k = (l + r) // 2
@@ -18,4 +36,4 @@ class Solution:
                 r = k
             else:
                 l = k + 1
-        return r
+        return r.__int__()
