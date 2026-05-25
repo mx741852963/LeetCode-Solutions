c7c020cfb8db545c6ec0c15b1e1f0081305ba052
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-25 20:10:11 +0300

    0875-koko-eating-bananas Medium Array 169 ms (54.4%) 20.6 MB (43.31%)

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,25 +1,3 @@
-# class Solution:
-#     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-#         def dose_k(k):
-#             hours = 0
-#             for p in piles:
-#                 hours += ceil(p / k)
-#             return hours <= h
-#         if len(piles) == 1 and piles[0]==h :
-#             return piles[0]
-#         if len(piles) == h:
-#             return max(piles)
-#         l = ceil(sum(piles)/h)
-#         r = max(piles)
-#         while l < r:
-#             k = (l + r) // 2
-#             if dose_k(k):
-#                 r = k
-#             else:
-#                 l = k + 1
-#         return r
-# Time O(n*log(r))
-# Space O(1)
 import math
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
