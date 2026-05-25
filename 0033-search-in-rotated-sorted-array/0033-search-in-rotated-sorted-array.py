c5bce49174012215dd9d554c6f2a40293af8f9b8
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-25 11:36:13 +0300

    0033-search-in-rotated-sorted-array Medium Array 0 ms (100%) 19.4 MB (78.14%)

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -11,17 +11,19 @@ class Solution:
                 r = min_index
         minn = l
         if minn == 0:
-            l,r = 0,lenn-1
+            l, r = 0, lenn - 1
         elif nums[0] <= target <= nums[minn - 1]:
-            l,r = 0,minn-1
-        else :
-            l,r = minn , lenn-1
-        while l<=r:
-            mid = (l+r)//2
+            l, r = 0, minn - 1
+        else:
+            l, r = minn, lenn - 1
+        while l <= r:
+            mid = (l + r) // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] > target:
-                r = mid -1
-            else :
+                r = mid - 1
+            else:
                 l = mid + 1
-        return  -1 
+        return -1 
+# Time = O(log n )
+# Space = O(1)
