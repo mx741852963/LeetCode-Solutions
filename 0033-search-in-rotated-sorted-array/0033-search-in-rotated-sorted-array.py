db4d2228ab06609a539cbba5e00cc97982bcdf29
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-25 11:57:26 +0300

    0033-search-in-rotated-sorted-array Medium Array 0 ms (100%) 19.5 MB (39.75%)

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,29 +1,47 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        lenn = len(nums)
-        l = 0
-        r = lenn - 1
-        while l < r:
-            min_index = (l + r) // 2
-            if nums[min_index] > nums[r]:
-                l = min_index + 1
-            else:
-                r = min_index
-        minn = l
-        if minn == 0:
-            l, r = 0, lenn - 1
-        elif nums[0] <= target <= nums[minn - 1]:
-            l, r = 0, minn - 1
-        else:
-            l, r = minn, lenn - 1
+        l, r = 0, len(nums) - 1
         while l <= r:
             mid = (l + r) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] > target:
-                r = mid - 1
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
             else:
-                l = mid + 1
-        return -1 
-# Time = O(log n )
-# Space = O(1)
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         lenn = len(nums)
+#         l = 0
+#         r = lenn - 1
+#         while l < r:
+#             min_index = (l + r) // 2
+#             if nums[min_index] > nums[r]:
+#                 l = min_index + 1
+#             else:
+#                 r = min_index
+#         minn = l
+#         if minn == 0:
+#             l, r = 0, lenn - 1
+#         elif nums[0] <= target <= nums[minn - 1]:
+#             l, r = 0, minn - 1
+#         else:
+#             l, r = minn, lenn - 1
+#         while l <= r:
+#             mid = (l + r) // 2
+#             if nums[mid] == target:
+#                 return mid
+#             elif nums[mid] > target:
+#                 r = mid - 1
+#             else:
+#                 l = mid + 1
+#         return -1 
+# # Time = O(log n )
+# # Space = O(1)
