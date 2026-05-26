9134a1f9b7fb62608525c0e8bf2532263c2916c6
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-26 11:47:12 +0300

    0021-merge-two-sorted-lists Easy Linked List 0 ms (100%) 19.3 MB (73.2%)

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -9,14 +9,10 @@ class Solution:
         cur = d 
         while list1 and list2:
             if list1.val < list2.val:
-                cur.next = list1
-                cur = list1
+                cur.next,cur = list1 , list1
                 list1 = list1.next
-
-
             else :
-                cur.next = list2
-                cur = list2
+                cur.next,cur = list2 , list2
                 list2 = list2.next
         cur.next = list1 if list1 else list2
         return d.next
