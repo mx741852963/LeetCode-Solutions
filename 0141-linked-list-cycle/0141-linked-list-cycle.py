12e556599096febc8719b90431cd827fa2b90d49
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-26 12:38:03 +0300

    0141-linked-list-cycle Easy Hash Table 50 ms (77.86%) 22.6 MB (27.29%)

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,13 +6,26 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        sett = set()
-        cur = head 
-        while cur :
-            d = id(cur)
-            if  d not in sett:
-                sett.add(d)
-                cur = cur.next
-            else :
-                return True
+        # sett = set()
+        # cur = head 
+        # while cur :
+        #     d = id(cur)
+        #     if  d not in sett:
+        #         sett.add(d)
+        #         cur = cur.next
+        #     else :
+        #         return True
+        # return False
+        # Time O(n)
+        # Space O(n)
+        dummy = ListNode()
+        dummy.next = head
+        slow = fast = dummy
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                return True 
         return False
+        # Time O(n)
+        # Space O(1)
