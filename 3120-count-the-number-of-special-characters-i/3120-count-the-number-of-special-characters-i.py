9c33137696a28498c763633df6bedd13a5aed826
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-26 14:49:40 +0300

    3120-count-the-number-of-special-characters-i Easy Hash Table 0 ms (100%) 19.5 MB (25.31%)

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -1,12 +1,10 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        # sett = set()
-        # for i in word :
-        #     if i.islower() and i.upper() in word :
-        #         sett.add(i)
-        # return len(sett)
+        sett = set(word)
+        count = 0
+        for i in sett:
+            if i.islower() and i.upper() in sett:
+                count += 1
+        return count
         # # Time O(n)
         # # space O(1)
-        sett = set()
-        [sett.add(i) for i in word  if i.islower() and i.upper() in word]
-        return len(sett)
