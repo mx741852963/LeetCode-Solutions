07ab4a2bf15c30a1e6906836cb007eeecfe31424
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-27 17:41:02 +0300

    3121-count-the-number-of-special-characters-ii Medium Hash Table 204 ms (85.29%) 21.2 MB (95.29%)

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -1,17 +1,31 @@
 from collections import defaultdict
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        count = 0
-        lower = defaultdict(list)
-        upper = defaultdict(list)
-        for i in range(len(word)):
-            if word[i].islower():
-                lower[word[i]].append(i)
+        # count = 0
+        # lower = defaultdict(list)
+        # upper = defaultdict(list)
+        # for i in range(len(word)):
+        #     if word[i].islower():
+        #         lower[word[i]].append(i)
+        #     else:
+        #         upper[word[i]].append(i)
+        # for i in lower:
+        #     j = i.upper()
+        #     if j in upper and max(lower[i]) < min(upper[j]) :
+        #         count += 1
+        # count
+        # return count
+        last_lower = {}
+        first_upper = {}
+        for index, char in enumerate(word):
+            if char.islower():
+                last_lower[char] = index
             else:
-                upper[word[i]].append(i)
-        for i in lower:
-            j = i.upper()
-            if j in upper and max(lower[i]) < min(upper[j]) :
+                if char not in first_upper:
+                    first_upper[char] = index
+        count = 0
+        for char, last_low_idx in last_lower.items():
+            up_char = char.upper()
+            if up_char in first_upper and last_low_idx < first_upper[up_char]:
                 count += 1
-        count
         return count
