c40abb802b554fbf0e39ff32bbddf33b0a5e5f41
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-28 19:20:06 +0300

    3093-longest-common-suffix-queries Hard Array 1321 ms (61.01%) 165.1 MB (74.21%)

---
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.py
@@ -0,0 +1,28 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.smallest = inf
+        self.index = inf
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    def insert(self,s,index):
+        cur = self.root
+        lenn = len(s)
+        if lenn < cur.smallest:cur.smallest,cur.index = lenn,index
+        for c in s :
+            if not c in cur.children : cur.children[c] = TrieNode()
+            cur = cur.children[c]
+            if lenn < cur.smallest:cur.smallest,cur.index = lenn,index
+    def query(self,s):
+        cur  = self.root
+        for c in s :
+            if not c in cur.children : break 
+            cur = cur.children[c]
+        return cur.index
+class Solution:
+    def stringIndices(self, wordsContainer: List[str], wordsQuery: List[str]) -> List[int]:
+        trie,res = Trie(),[]
+        for i ,j in enumerate(wordsContainer):trie.insert(j[::-1],i)
+        for i in wordsQuery :res.append(trie.query(i[::-1]))
+        return res
