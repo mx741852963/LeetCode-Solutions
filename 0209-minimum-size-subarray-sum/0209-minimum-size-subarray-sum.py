b03ce6baa20041f93849a43b7acb6b3d774d540e
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-29 19:36:09 +0300

    0209-minimum-size-subarray-sum Medium Array 7 ms (94.19%) 30.6 MB (42.25%)

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l = 0
+        lenn = len (nums)
+        minn = float('inf')
+        count = 0
+        if target in nums:
+            return 1
+        for r in range(lenn):
+            count += nums[r]
+            while count >= target:
+                minn = min(minn,r-l+1)
+                count -=nums[l]
+                l +=1
+        return minn if minn !=float('inf') else 0
