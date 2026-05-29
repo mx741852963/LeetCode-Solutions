2153a987d9f794c0507a55655e4ab313398f30dc
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-29 17:54:03 +0300

    0424-longest-repeating-character-replacement Medium Hash Table 47 ms (99.78%) 19.7 MB (50.58%)

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,13 +1,15 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = [0]*26  
+        count = [0] * 26  
         l = 0
-        maxx = 0
         lenn = len(s)
+        max_freq = 0  
         for r in range(lenn):
-            count[ord(s[r])-65]  += 1
-            while (r - l + 1) - max(count) > k :
-                count[ord(s[l])-65] -= 1
+            idx = ord(s[r]) - 65
+            count[idx] += 1
+            if count[idx] > max_freq:
+                max_freq = count[idx]
+            if (r - l + 1) - max_freq > k:
+                count[ord(s[l]) - 65] -= 1
                 l += 1
-            maxx = max(maxx, (r - l + 1))
-        return maxx
+        return lenn - l
