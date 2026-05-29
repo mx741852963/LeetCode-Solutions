007b8d5e12895b7c57d9217b67833d446bff86b0
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-29 20:25:39 +0300

    0567-permutation-in-string Medium Hash Table 12 ms (88.14%) 19.4 MB (53.41%)

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        len1,len2 = len(s1), len(s2)
+        if len1 > len2:
+            return False
+        wight_of_s1 = [0]*26
+        wight_of_s2 = [0]*26
+        for i in range(len1):
+            wight_of_s1[ord(s1[i])-97] +=1
+            wight_of_s2[ord(s2[i])-97] +=1
+        if wight_of_s1==wight_of_s2:
+                return True
+        for r in range(len1, len2):
+            wight_of_s2[ord(s2[r])-97] +=1
+            wight_of_s2[ord(s2[r-len1])-97] -=1 
+            if wight_of_s1==wight_of_s2:
+                return True
+        return False
