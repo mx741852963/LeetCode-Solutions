74d1e22a4656429c69861a87fa08c20ec2a17b10
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-29 10:23:20 +0300

    3300-minimum-element-after-replacement-with-digit-sum Easy Array 3 ms (80.35%) 19.3 MB (21.61%)

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
@@ -1,12 +1,23 @@
 class Solution:
     def minElement(self, nums: List[int]) -> int:
+        # minn = float("inf")
+        # def collect_res(num):
+        #     res = 0
+        #     string = str(num)
+        #     for i in string:
+        #         res += int(i)
+        #     return res
+        # for num in list(map(collect_res,nums)):
+        #    minn=min(minn,num)
+        # return minn
         minn = float("inf")
-        def collect_res(num):
-            res = 0
-            string = str(num)
-            for i in string:
-                res += int(i)
-            return res
-        for num in list(map(collect_res,nums)):
-           minn=min(minn,num)
+        for num in nums:
+            summ = 0
+            while num > 0:
+                summ  +=  num % 10
+                num //=10
+            if summ < minn:
+                minn = summ
         return minn
+# Time O(n)
+# Space O(1)
