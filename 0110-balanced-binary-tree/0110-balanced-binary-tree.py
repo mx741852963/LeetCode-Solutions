4e7c344decdf2b801ebc3fb8ee3f814a421074be
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-30 11:41:15 +0300

    0110-balanced-binary-tree Easy Tree 0 ms (100%) 20.4 MB (70.96%)

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,17 +6,27 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        flag = [True]
-        def height(root):
-            if not root:return 0
-            left_h = height(root.left)
-            if flag[0] is False:return 0
-            right_h = height(root.right)
-            if abs(left_h-right_h)>1:
-                flag[0] = False
-                return 0 
-            return  1 +max(left_h,right_h)
-        height(root)
-        return flag[0]
+        # flag = [True]
+        # def height(root):
+        #     if not root:return 0
+        #     left_h = height(root.left)
+        #     if flag[0] is False:return 0
+        #     right_h = height(root.right)
+        #     if abs(left_h-right_h)>1:
+        #         flag[0] = False
+        #         return 0 
+        #     return  1 +max(left_h,right_h)
+        # height(root)
+        # return flag[0]
+        def check_height(node):
+            if not node:return 0  
+            left_height = check_height(node.left)
+            if left_height == -1: return -1  
+            right_height = check_height(node.right)
+            if right_height == -1: return -1  
+            if abs(left_height - right_height) > 1:return -1  
+            return 1 + max(left_height, right_height)
+        return check_height(root) != -1
+        check_height(root)
     # Time O(n)
     # Space O(h)
