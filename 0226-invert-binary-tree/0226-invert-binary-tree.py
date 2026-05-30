3726e589f336c45aedd61774e132d49849fdf279
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-30 09:55:58 +0300

    0226-invert-binary-tree Easy Tree 0 ms (100%) 19.3 MB (21.73%)

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -8,9 +8,9 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return None
-        root.left,root.right =root.right,root.left
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+        root.left,root.right =self.invertTree(root.right),self.invertTree(root.left)
+       
+        
         return root
 # Time O(n)
 # Space O(h)
