31a9fa3478c9622469f829dc23148e747a244e32
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-30 10:48:17 +0300

    0104-maximum-depth-of-binary-tree Easy Tree 0 ms (100%) 20.1 MB (91.74%)

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -4,9 +4,21 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:return 0
-        return 1+max(self.maxDepth(root.left),self.maxDepth(root.right))  
+        q = deque([root])
+        depth = 0
+        while q:
+            level_size = len(q)
+            for _ in range(level_size):
+                node = q.popleft()
+                if node.left :q.append(node.left)
+                if node.right : q.append(node.right)
+            depth+=1
+        return depth
+        # if not root:return 0
+        # return 1+max(self.maxDepth(root.left),self.maxDepth(root.right))  
 # Time O(n)
 # Space O(h)
