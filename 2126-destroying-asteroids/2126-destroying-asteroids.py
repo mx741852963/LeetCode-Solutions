92146c1f59746c2c8580f10875f05c5301812224
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-31 11:24:17 +0300

    2126-destroying-asteroids Medium Array 73 ms (58.93%) 34.1 MB (12.48%)

--- 2126-destroying-asteroids/2126-destroying-asteroids.py
+++ 2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -8,6 +8,8 @@ class Solution:
                 return False
             summ += asteroids[i]
         return True
+        # Time O(n log n)
+        # space O(n)
         # if asteroids[0]<=mass:
         #     summ = mass + asteroids[0]
         #     for  i in range(1,len(asteroids)):
