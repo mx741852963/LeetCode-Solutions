9f6ef23bb0255eb723059c071e55a1a3f4b64777
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-05-31 11:23:16 +0300

    2126-destroying-asteroids Medium Array 95 ms (21.3%) 34.1 MB (30.13%)

--- 2126-destroying-asteroids/2126-destroying-asteroids.py
+++ 2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -1,12 +1,18 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
         asteroids.sort()
-        summ = 0
-        if asteroids[0]<=mass:
-            summ = mass + asteroids[0]
-            for  i in range(1,len(asteroids)):
-                if asteroids[i]>summ:
-                    return False
-                summ += asteroids[i]
-            return True 
-        return False
+        summ = mass
+        lenn=len(asteroids)
+        for i in range(lenn):
+            if asteroids[i] > summ:
+                return False
+            summ += asteroids[i]
+        return True
+        # if asteroids[0]<=mass:
+        #     summ = mass + asteroids[0]
+        #     for  i in range(1,len(asteroids)):
+        #         if asteroids[i]>summ:
+        #             return False
+        #         summ += asteroids[i]
+        #     return True 
+        # return False
