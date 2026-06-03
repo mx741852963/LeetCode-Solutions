4fa63b22715a85963fcc967f9de300968708c5a7
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-03 11:23:36 +0300

    3635-earliest-finish-time-for-land-and-water-rides-ii Medium Array 105 ms (92.73%) 33.5 MB (90.91%)

---
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.py
@@ -0,0 +1,8 @@
+import heapq
+class Solution:
+    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
+        min_end_land = min(s + d for s, d in zip(landStartTime, landDuration))
+        ans_land_water = min(max(min_end_land, s) + d for s, d in zip(waterStartTime, waterDuration))
+        min_end_water = min(s + d for s, d in zip(waterStartTime, waterDuration))
+        ans_water_land = min(max(min_end_water, s) + d for s, d in zip(landStartTime, landDuration))
+        return min(ans_land_water, ans_water_land)
