acd86150aa6dab6112e2d725d302360c8de7dcd6
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-03 13:27:41 +0300

    0100-same-tree Easy Tree 0 ms (100%) 19.3 MB (31.28%)

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -7,7 +7,7 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q: return True
-        if (p and not q) or (q and not p): return False
+        if not p or not q: return False
         if p.val!=q.val:return False
         return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
     # Time O(min(n,m))
