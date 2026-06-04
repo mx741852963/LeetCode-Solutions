84355fb857616a7d1e73ba7b4bf512d31751e9da
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-04 14:01:20 +0300

    3751-total-waviness-of-numbers-in-range-i Medium Math 271 ms (70.12%) 19.4 MB (32.95%)

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.py
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.py
@@ -1,22 +1,36 @@
 class Solution:
     def totalWaviness(self, num1: int, num2: int) -> int:
-        count = 0
+        wave  = 0
         if num1 < 100:
-            num1 =101
+            num1 = 101
         for i in range(num1, num2 + 1):
-            num = i
-            res = []
-            while num > 0:
-                res.append(num % 10)
-                num = num // 10
-                j = 0
-            while j < len(res) - 2:
-                l = res[j]
-                m = res[j + 1]
-                r = res[j + 2]
-                if m > max(r,l) or m < min(l,r):
-                    count += 1
+            num = str(i)
+            j = 0
+            while j < len(num) - 2:
+                l = num[j]
+                m = num[j + 1]
+                r = num[j + 2]
+                if m > max(r, l) or m < min(l, r):
+                    wave += 1
                 j += 1
-        return count
-# Time O(n log m)
-# Space O( log m)
+        return wave
+#         count = 0
+#         if num1 < 100:
+#             num1 =101
+#         for i in range(num1, num2 + 1):
+#             num = i
+#             res = []
+#             while num > 0:
+#                 res.append(num % 10)
+#                 num = num // 10
+#                 j = 0
+#             while j < len(res) - 2:
+#                 l = res[j]
+#                 m = res[j + 1]
+#                 r = res[j + 2]
+#                 if m > max(r,l) or m < min(l,r):
+#                     count += 1
+#                 j += 1
+#         return count
+# # Time O(n log m)
+# # Space O( log m)
