e78477399cad8e3170203db87ff3539b1aca38e8
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-04 11:36:10 +0300

    3751-total-waviness-of-numbers-in-range-i Medium Math 328 ms (57.86%) 19.3 MB (69.73%)

---
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalWaviness(self, num1: int, num2: int) -> int:
+        count = 0
+        for i in range(num1, num2 + 1):
+            num = i
+            res = []
+            if i < 100:
+                continue
+            while num > 0:
+                res.append(num % 10)
+                num = num // 10
+                j = 0
+            while j < len(res) - 2:
+                if (res[j] > res[j + 1] < res[j + 2]) or (res[j] < res[j + 1] > res[j + 2]):
+                    count += 1
+                j += 1
+        return count
