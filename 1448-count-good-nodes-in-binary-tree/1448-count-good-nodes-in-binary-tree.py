01ab48b8ee4eb951fdf1b306af1c674687a63136
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-04 20:44:08 +0300

    1448-count-good-nodes-in-binary-tree Medium Tree 129 ms (56.09%) 32.2 MB (6.03%)

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -0,0 +1,21 @@
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        if not root:return 0
+        q = deque()
+        q.append((root,root.val))
+        count = 0
+        while q: 
+            node,maxx = q.popleft()
+            if maxx <= node.val: count +=1
+            maxx = max(maxx,node.val)
+            if node.left : q.append((node.left,maxx))
+            if node.right : q.append((node.right,maxx))
+        return count
+        
