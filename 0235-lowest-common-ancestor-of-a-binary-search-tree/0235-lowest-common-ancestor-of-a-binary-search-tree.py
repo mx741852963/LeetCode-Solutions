64433089988a2479db007970ed6f8843f34f6957
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-05 20:36:19 +0300

    0235-lowest-common-ancestor-of-a-binary-search-tree Medium Tree 71 ms (21.13%) 23 MB (17.83%)

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,13 +7,8 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.lca = root
         def search(node):
-            if not node : return 
-            self.lca = node
-            if node is p or node is q:return 
-            elif node.val < p.val and node.val < q.val:search(node.right)
-            elif node.val > p.val and node.val > q.val:search(node.left)
-            else:return 
-        search(root)
-        return self.lca
+            if node.val < p.val and node.val < q.val: return search(node.right)
+            elif node.val > p.val and node.val > q.val: return search(node.left)
+            else:return node
+        return search(root)
