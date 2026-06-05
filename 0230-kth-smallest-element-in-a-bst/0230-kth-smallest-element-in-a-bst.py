70f3720392a7fc21788d41f25a44212d5b21acb4
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-05 19:59:13 +0300

    0230-kth-smallest-element-in-a-bst Medium Tree 0 ms (100%) 22.2 MB (60.76%)

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.k = k
+        self.ans = 0
+        def dfs(node):
+            if not node : return 
+            dfs(node.left)
+            if self.k == 1:
+                self.ans = node.val
+            self.k -=1
+            if self.k > 0 :
+                dfs(node.right)
+        dfs(root)
+        return self.ans
