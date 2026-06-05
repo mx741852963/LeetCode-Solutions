ab7304fd03946abd16ddfabd19021059de9f6f49
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-05 19:19:08 +0300

    0208-implement-trie-prefix-tree Medium Hash Table 317 ms (5.04%) 26.4 MB (98.5%)

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -0,0 +1,25 @@
+class Trie:
+
+    def __init__(self):
+        self.trie = set()
+
+    def insert(self, word: str) -> None:
+        self.trie.add(word)
+        return None
+
+    def search(self, word: str) -> bool:
+        return True if word in self.trie else False
+
+    def startsWith(self, prefix: str) -> bool:
+        for word in self.trie:
+            if word.startswith(prefix):
+                return True
+            
+        return False 
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
