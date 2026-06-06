4d6d5175d4045a472dc8a0607df5e8e9db4fc545
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-06 11:44:29 +0300

    0112-path-sum Easy Tree 0 ms (100%) 19.8 MB (99.77%)

---
+++ 0112-path-sum/0112-path-sum.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def dfs(node,summ):
+            if not node :return False
+            summ +=node.val
+            if not node.left and not node.right: return summ==targetSum 
+            return dfs(node.right,summ) or dfs(node.left,summ)
+        return dfs(root,0)
