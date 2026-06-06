9e46670c8e0c9c999fb0ef48cdd33e9c4e1a17a1
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-06 08:54:01 +0300

    2574-left-and-right-sum-differences Easy Array 0 ms (100%) 19.5 MB (23.2%)

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
@@ -1,6 +1,14 @@
 class Solution:
     def leftRightDifference(self, nums: List[int]) -> List[int]:
-        lenn=len(nums)
-        l_s,r_s = [0] * lenn,[0] * lenn
-        for i in range(lenn - 1):l_s[i + 1],r_s[-i - 2] = nums[i] + l_s[i],nums[-i - 1] + r_s[-i - 1]
-        return [abs(x - y) for x, y in zip(l_s, r_s)]
+        # lenn=len(nums)
+        # l_s,r_s = [0] * lenn,[0] * lenn
+        # for i in range(lenn - 1):l_s[i + 1],r_s[-i - 2] = nums[i] + l_s[i],nums[-i - 1] + r_s[-i - 1]
+        # return [abs(x - y) for x, y in zip(l_s, r_s)]
+        left_sum,right_sum,ans  = 0,sum(nums),[] 
+        for num in nums:
+            right_sum -= num  
+            ans.append(abs(left_sum - right_sum))
+            left_sum += num 
+        return ans
+# Time O(n) 
+# space O(1)  
