8f5f6f549f349c95b6e4f6ceb6758f8aab8611fa
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-09 10:07:56 +0300

    3689-maximum-total-subarray-value-i Medium Array 5 ms (92%) 26.5 MB (60%)

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.py
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.py
@@ -1,4 +1,3 @@
 class Solution:
     def maxTotalValue(self, nums: List[int], k: int) -> int:
-        sub = max(nums) - min(nums)
-        return sub * k 
+        return (max(nums) - min(nums)) * k 
