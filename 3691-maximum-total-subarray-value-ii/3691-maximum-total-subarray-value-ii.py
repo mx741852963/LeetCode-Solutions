897b6c4aef165ba4ae391a418a706c79deaf234d
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-10 09:10:59 +0300

    3691-maximum-total-subarray-value-ii Hard Array 4112 ms (51.22%) 51.1 MB (53.66%)

--- 3691-maximum-total-subarray-value-ii/3691-maximum-total-subarray-value-ii.py
+++ 3691-maximum-total-subarray-value-ii/3691-maximum-total-subarray-value-ii.py
@@ -1,5 +1,7 @@
 class Solution:
     def maxTotalValue(self, nums: List[int], k: int) -> int:
+        # Time O(n log n ) 
+        # Spcae O(n log n )
         def buildSparseTableMax(arr):
             n = len(arr)
             lookup = [[0] * (int(math.log(n, 2)) + 1) for _ in range(n + 1)]
@@ -13,7 +15,8 @@ class Solution:
                         lookup[i][j] = lookup[i + (1 << (j - 1))][j - 1]
             return lookup
 
-
+        # Time O(n log n ) 
+        # Spcae O(n log n )
         def buildSparseTableMin(arr):
             n = len(arr)
             lookup = [[0] * (int(math.log(n, 2)) + 1) for _ in range(n + 1)]
@@ -27,7 +30,7 @@ class Solution:
                         lookup[i][j] = lookup[i + (1 << (j - 1))][j - 1]
             return lookup
 
-
+        # Time O(1)
         def query_min(L, R, lookup):
             j = int(math.log(R - L + 1, 2))
             if lookup[L][j] <= lookup[R - (1 << j) + 1][j]:
@@ -35,7 +38,7 @@ class Solution:
             else:
                 return lookup[R - (1 << j) + 1][j]
 
-
+        # Time O(1)
         def query_max(L, R, lookup):
             j = int(math.log(R - L + 1, 2))
             if lookup[L][j] >= lookup[R - (1 << j) + 1][j]:
@@ -50,12 +53,15 @@ class Solution:
         r = lenn - 1
         count = 0
         heap = []
+        # Time O(n log n ) 
+        # Spcae O(log n )
         for i in range(lenn):
             maxx = query_max(i, lenn - 1, lookup_max)
             minn = query_min(i, lenn - 1, lookup_min)
             j = maxx - minn
             heapq.heappush(heap, (-j, i, r))
         m = 0
+        # Time O(k log n)
         while k:
             num, m, r = heapq.heappop(heap)
             count += num
@@ -67,3 +73,5 @@ class Solution:
                 heapq.heappush(heap, (-j, m, r))
             k -= 1
         return -count
+# Time O((n+k) log n)
+# Spcae O(n log n )
