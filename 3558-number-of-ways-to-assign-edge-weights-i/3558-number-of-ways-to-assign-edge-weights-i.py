9791471a6f8bb7dbf61b3abd06f3eeb58dcdf7fe
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-11 08:35:37 +0300

    3558-number-of-ways-to-assign-edge-weights-i Medium Math 831 ms (5.38%) 69.5 MB (59.14%)

---
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def assignEdgeWeights(self, edges: List[List[int]]) -> int:
+        adjacency_list = defaultdict(list)
+        for u, v in edges:
+            adjacency_list[u].append(v)
+            adjacency_list[v].append(u)
+        seen = set()
+        seen.add(1)
+        q = deque([(1, 0)])
+        max_depth = 0
+        while q:
+            node, depth = q.popleft()
+            print(node, depth)
+            for nei_node in adjacency_list[node]:
+                if nei_node not in seen:
+                    seen.add(nei_node)
+                    q.append((nei_node, depth + 1))
+            max_depth = max(depth, max_depth)
+        return pow(2, max_depth - 1, 10**9 + 7)
