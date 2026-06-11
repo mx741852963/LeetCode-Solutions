1bd640d2179aeaf2efc45f730fcb0622e5a5ff1c
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-11 08:37:40 +0300

    3558-number-of-ways-to-assign-edge-weights-i Medium Math 455 ms (21.5%) 69.2 MB (62.37%)

--- 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.py
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.py
@@ -10,7 +10,6 @@ class Solution:
         max_depth = 0
         while q:
             node, depth = q.popleft()
-            print(node, depth)
             for nei_node in adjacency_list[node]:
                 if nei_node not in seen:
                     seen.add(nei_node)
