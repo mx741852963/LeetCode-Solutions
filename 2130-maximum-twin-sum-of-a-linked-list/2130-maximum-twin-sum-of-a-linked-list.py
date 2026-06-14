05209c0a46a12f1b032e6f5b0c94e13f8eb0b09c
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-14 19:49:07 +0300

    2130-maximum-twin-sum-of-a-linked-list Medium Linked List 62 ms (62.23%) 50.6 MB (57.14%)

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -5,15 +5,25 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        mapp = []
-        while head:
-            mapp.append(head.val)
-            head = head.next
-            
-        n = len(mapp)
-        summ = 0
-        j = n - 1 
-        for i in range(n//2):
-            summ = max(summ,mapp[i] + mapp[j])
-            j -=1
+        # mapp = []
+        # while head:
+        #     mapp.append(head.val)
+        #     head = head.next 
+        # n = len(mapp)
+        # summ = 0
+        # j = n - 1 
+        # for i in range(n//2):
+        #     summ = max(summ,mapp[i] + mapp[j])
+        #     j -=1
+        # return summ
+        slow,fast,start,prev,summ = head,head,head,None,0
+        while fast and fast.next:
+            slow,fast = slow.next,fast.next.next
+        while slow:
+            nxt,slow.next = slow.next,prev
+            prev,slow = slow,nxt
+        while prev:
+            summ= max(summ,(start.val+prev.val))
+            prev,start = prev.next,start.next
         return summ
+
