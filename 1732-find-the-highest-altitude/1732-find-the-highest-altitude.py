4d5c5d99d1a4e287b2bbc99f5515baa0445abd92
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-19 07:56:33 +0300

    1732-find-the-highest-altitude Easy Array 0 ms (100%) 19.4 MB (15.4%)

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -1,8 +1,18 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        n  = len(gain)
-        for i in range(n):
-            if i == 0: continue
-            gain[i] = gain[i] + gain[i - 1]
-        maxx =  max(gain)
-        return maxx if maxx >=0 else 0
+        # n  = len(gain)
+        # maxx = 0
+        # for i in range(1,n):
+        #     gain[i] = gain[i] + gain[i - 1] 
+        # maxx =  max(gain)
+        # return maxx if maxx >=0 else 0
+# Time O(n)
+# Space O(1)
+        maxx = 0 
+        cur_altitude = 0
+        for g in gain :
+            cur_altitude += g
+            if cur_altitude > maxx : maxx = cur_altitude
+        return maxx
+# Time O(n)
+# Space O(n)
