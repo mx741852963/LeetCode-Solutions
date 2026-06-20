98457df8dbb0a08b62d9db93f68b66882ec6109c
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-20 15:15:44 +0300

    1046-last-stone-weight Easy Array 0 ms (100%) 19.7 MB (9.48%)

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,12 +1,13 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        n = len(stones)
+        n = stones.__len__()
         for i in range(n):stones[i] = -stones[i]
         heapq.heapify(stones)
         while len(stones) > 1 :
             largest = heapq.heappop(stones)
             next_largest = heapq.heappop(stones)
             if largest != next_largest:heapq.heappush(stones, largest-next_largest)
-
         if len(stones)==1:return -stones[-1]
         else:return 0
+# Time O(n log n)
+# Space O(n)
