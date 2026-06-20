3f97fdc4792439e114264bc6bd23058302421d5e
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-20 17:21:59 +0300

    0347-top-k-frequent-elements Medium Array 0 ms (100%) 23.1 MB (31.79%)

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,3 +1,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return [Counter(nums).most_common(k)[i][0] for i in range(k)]
+        # return [Counter(nums).most_common(k)[i][0] for i in range(k)] 3 second
+        counter = Counter(nums)
+        min_heap = []
+        for key, value in counter.items():
+            if len(min_heap) < k:
+                heapq.heappush(min_heap, (value, key))
+            else:
+                heapq.heappushpop(min_heap, (value, key))
+        return [min_heap[i][1] for i in range(k)]
