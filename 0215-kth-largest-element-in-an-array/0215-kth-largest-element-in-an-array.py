3da5b3affe1a639eafc19da94ceb4b97b8da43bb
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-20 16:51:41 +0300

    0215-kth-largest-element-in-an-array Medium Array 143 ms (8.93%) 31 MB (36.14%)

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,15 +1,25 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        for i in range(len(nums)):nums[i] = -nums[i]
-        heapq.heapify(nums)
-        while k > 1:
-            k -= 1
-            heapq.heappop(nums)
-        return -nums[0]
+        # for i in range(len(nums)):nums[i] = -nums[i]
+        # heapq.heapify(nums)
+        # while k > 1:
+        #     k -= 1
+        #     heapq.heappop(nums)
+        # return -nums[0]
 # Time O(n + K log n)
 # Space O(1)
 # 
-        while k > 1:
-            k -= 1
-            nums[nums.index(max(nums))] = 0
-        return max(nums)
+# Time Limit Exceeded
+        # while k > 1:
+        #     k -= 1
+        #     nums[nums.index(max(nums))] =  -float("inf")
+        # return max(nums)
+# 
+        min_heap = []
+        for num in nums:
+            if len(min_heap) < k:
+                print(num)
+                heapq.heappush(min_heap, num)
+            else:
+                heapq.heappushpop(min_heap, num)
+        return min_heap[0]
