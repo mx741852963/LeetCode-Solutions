85873c81b51131be766ec278e4516e79a1a09f4d
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-20 18:44:44 +0300

    0973-k-closest-points-to-origin Medium Array 55 ms (74.74%) 24.7 MB (62.83%)

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -4,9 +4,15 @@ class Solution:
         heapq.heapify(res)
         for i in range(len(points)):
             if len(res) < k:
-                heapq.heappush(res, (-(points[i][0] ** 2 + points[i][1] ** 2),[points[i][0],points       [i][1]]))
+                heapq.heappush(
+                    res, (-(points[i][0] ** 2 + points[i][1] ** 2), points[i])
+                )
             else:
-                heapq.heappushpop(res, (-(points[i][0] ** 2 + points[i][1] ** 2),[points[i][0],      points[i][1]]))
+                heapq.heappushpop(
+                    res, (-(points[i][0] ** 2 + points[i][1] ** 2), points[i])
+                )
         return [res[i][1] for i in range(k)]
+
+
 # Time O(n log k)
 # Space O(k)
