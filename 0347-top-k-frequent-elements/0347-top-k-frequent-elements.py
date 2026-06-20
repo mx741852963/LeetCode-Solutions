a7daf1e593d82bca47d832a9e27528d387ecd899
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-20 17:54:03 +0300

    0347-top-k-frequent-elements Medium Array 7 ms (51.91%) 22.6 MB (94.7%)

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,27 +3,26 @@ class Solution:
         #  Solution one 
         # return [Counter(nums).most_common(k)[i][0] for i in range(k)] 3 second
         #  Solution two
-        counter = Counter(nums)
-        min_heap = []
-        for key, value in counter.items():
-            if len(min_heap) < k:
-                heapq.heappush(min_heap, (value, key))
-            else:
-                heapq.heappushpop(min_heap, (value, key))
-        return [min_heap[i][1] for i in range(k)] # 0ms
+        # counter = Counter(nums)
+        # min_heap = []
+        # for key, value in counter.items():
+        #     if len(min_heap) < k:
+        #         heapq.heappush(min_heap, (value, key))
+        #     else:
+        #         heapq.heappushpop(min_heap, (value, key))
+        # return [min_heap[i][1] for i in range(k)] # 0ms
         # Time O(n * log K)
         # space O(n)
         #  Solution three
-        # n= len(nums)
-        # ans = []
-        # res = [0]* (n+1)
-        # counter = Counter(nums)
-        # for key, value in counter.items():
-        #     if res[value] ==0:res[value] = [key]
-        #     else:res[value].append(key)
-        # for i in range(n,-1,-1):
-        #     if res[i]!=0: ans.extend(res[i])
-        #     if len(ans)==k:break
-        # return ans #6ms
+        n= len(nums)
+        ans = []
+        res = [0]* (n+1)
+        counter = Counter(nums)
+        for key, value in counter.items():
+            if res[value] ==0:res[value] = [key]
+            else:res[value].append(key)
+        for i in range(n,-1,-1):
+            if res[i]!=0: ans.extend(res[i])
+        return ans[:k] #6ms
         # Time O(n)
         # space O(n)
