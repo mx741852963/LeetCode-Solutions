aa4f5dea3c826394163eac458efaab9b1d1a35da
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-20 11:33:04 +0300

    1840-maximum-building-height Hard Array 318 ms (6.82%) 46.4 MB (72.73%)

---
+++ 1840-maximum-building-height/1840-maximum-building-height.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxBuilding(self, n: int, restrictions: List[List[int]]) -> int:
+        r = restrictions
+        r.append([1, 0])
+        r.sort()
+        if r[-1][0]!= n:r.append([n,n-1])
+        lenn = len(r)
+        for i in range(1, lenn):
+            r[i][1] = min(r[i][1], r[i - 1][1] + (r[i][0] - r[i - 1][0]))
+        for i in range(lenn - 2, -1, -1):
+            r[i][1] = min(r[i][1], r[i + 1][1] + (r[i + 1][0] - r[i][0]))
+        res = 0
+        for i in range(lenn - 1):
+            tem = ((r[i+ 1][0 ] - r[i][0]) + r[i][1] + r[i + 1][1])//2
+            res = max(res,tem)
+        return res
