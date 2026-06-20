446276f6bec44a76d3428bceb6fc097fe07cc425
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-20 11:47:44 +0300

    1840-maximum-building-height Hard Array 236 ms (75%) 46.4 MB (54.55%)

--- 1840-maximum-building-height/1840-maximum-building-height.py
+++ 1840-maximum-building-height/1840-maximum-building-height.py
@@ -3,7 +3,8 @@ class Solution:
         r = restrictions
         r.append([1, 0])
         r.sort()
-        if r[-1][0]!= n:r.append([n,n-1])
+        if r[-1][0] != n:
+            r.append([n, n - 1])
         lenn = len(r)
         for i in range(1, lenn):
             r[i][1] = min(r[i][1], r[i - 1][1] + (r[i][0] - r[i - 1][0]))
@@ -11,6 +12,8 @@ class Solution:
             r[i][1] = min(r[i][1], r[i + 1][1] + (r[i + 1][0] - r[i][0]))
         res = 0
         for i in range(lenn - 1):
-            tem = ((r[i+ 1][0 ] - r[i][0]) + r[i][1] + r[i + 1][1])//2
-            res = max(res,tem)
+            tem = ((r[i + 1][0] - r[i][0]) + r[i][1] + r[i + 1][1]) // 2
+            res = max(res, tem)
         return res
+# Time O(n)
+# Space O(n)
