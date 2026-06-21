4f47c9b5758baafe7675b08f1fa67012902cd73a
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-21 13:00:22 +0300

    0347-top-k-frequent-elements Medium Array 7 ms (51.91%) 22.8 MB (81.29%)

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,6 +1,7 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         #  Solution one 
+        return [item[0] for item in Counter(nums).most_common(k)]
         # return [Counter(nums).most_common(k)[i][0] for i in range(k)] 3 second
         #  Solution two
         # counter = Counter(nums)
@@ -14,16 +15,16 @@ class Solution:
         # Time O(n * log K)
         # space O(n)
         #  Solution three
-        n= len(nums)
-        ans = []
-        res = [0]* (n+1)
-        counter = Counter(nums)
-        for key, value in counter.items():
-            if res[value] ==0:res[value] = [key]
-            else:res[value].append(key)
-        for i in range(n,-1,-1):
-            if res[i]!=0: ans.extend(res[i])
-            if len(ans) >= k:break
-        return ans[:k] #6ms
+        # n= len(nums)
+        # ans = []
+        # res = [0]* (n+1)
+        # counter = Counter(nums)
+        # for key, value in counter.items():
+        #     if res[value] ==0:res[value] = [key]
+        #     else:res[value].append(key)
+        # for i in range(n,-1,-1):
+        #     if res[i]!=0: ans.extend(res[i])
+        #     if len(ans) >= k:break
+        # return ans[:k] #6ms
         # Time O(n)
         # space O(n)
