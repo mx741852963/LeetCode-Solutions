98781560d67bd30517412e2ea8159eae889168cb
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-21 17:32:07 +0300

    0451-sort-characters-by-frequency Medium Hash Table 50 ms (5.22%) 20.4 MB (29.84%)

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,11 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        res = []
+        ans = ''
+        counter = Counter(s)
+        for char, frequency in counter.items():
+            heapq.heappush(res, (-frequency,char))
+        while len(res) != 0:
+            frequency,char = heapq.heappop(res)
+            ans += "".join(char) * -frequency
+        return ans
