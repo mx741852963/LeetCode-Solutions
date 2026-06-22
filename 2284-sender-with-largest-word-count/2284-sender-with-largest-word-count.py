8eb8510e2495d2325692693afd215173586a6872
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-22 18:51:30 +0300

    2284-sender-with-largest-word-count Medium Array 36 ms (68.47%) 24 MB (70.72%)

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.py
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.py
@@ -1,12 +1,17 @@
 class Solution:
     def largestWordCount(self, messages: List[str], senders: List[str]) -> str:
-        hash_map = []
         counter = defaultdict(int)
         for sender, message in zip(senders, messages):
             counter[sender]+=len(message.split())
-        for key , value in counter.items():
-            if len(hash_map) < 1:
-                heapq.heappush(hash_map, (value, key))
-            else:
-                heapq.heappushpop(hash_map, (value, key))
-        return hash_map[0][1]
+        max_word = ''
+        max_length = 0
+        for key,value in counter.items():
+            if value > max_length:
+                max_length = value
+                max_word = key
+            elif value == max_length:
+                if key > max_word:
+                    max_word = key           
+        return max_word   
+# Time O(n)
+# Spcae O(u)
