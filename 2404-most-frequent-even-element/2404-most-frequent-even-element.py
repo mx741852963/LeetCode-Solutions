a050d9256997cf712a7d3ec0021555743c2e8f94
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-22 21:09:31 +0300

    2404-most-frequent-even-element Easy Array 13 ms (91.87%) 19.7 MB (25.13%)

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.py
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.py
@@ -1,12 +1,9 @@
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
-        counter = Counter(nums)
-        heap = []
-        for key,value in counter.items():
-            if (key&1)==0:
-                if len(heap) < 1:
-                    heapq.heappush(heap, (value, -key))
-                else:heapq.heappushpop(heap, (value, -key))
-        if heap : return -heap[0][1] 
-        else : return -1
-         
+        counter,ans,max_freq = Counter(nums),-1,0
+        for key, value in counter.items():
+            if (key & 1) == 0:
+                if value > max_freq or (value == max_freq and key < ans): max_freq,ans = value,key         
+        return ans
+# Time O(n)
+# Space O(u)
