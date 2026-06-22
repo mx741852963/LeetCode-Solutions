2997d529f7d939387a3c1a33f7de7baefe482f00
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-22 09:17:03 +0300

    0659-split-array-into-consecutive-subsequences Medium Array 92 ms (12.26%) 20.4 MB (73.27%)

--- 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
@@ -1,12 +1,16 @@
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
-        # chains = defaultdict(list)
-        # for num in nums:
-        #     if chains[num - 1]:
-        #         length = heapq.heappop(chains[num - 1])
-        #         heapq.heappush(chains[num], length + 1)
-        #     else:
-        #         heapq.heappush(chains[num], 1)
+        chains = defaultdict(list)
+        for num in nums:
+            if chains[num - 1]:
+                length = heapq.heappop(chains[num - 1])
+                heapq.heappush(chains[num], length + 1)
+            else:
+                heapq.heappush(chains[num], 1)
+        for last_num in chains:
+            if chains[last_num] and chains[last_num][0] < 3:
+                return False
+        return True
         # for last_num in chains:
         #     for length in chains[last_num]:
         #         if length < 3:
@@ -14,17 +18,19 @@ class Solution:
         # return True
 # Time O(n log k)
 # Space O(n)
-        counter = collections.Counter(nums)
-        hash_map = collections.defaultdict(int)
-        for num in nums:
-            if counter[num]==0:continue
-            counter[num]-=1
-            if hash_map[num-1]>0:
-                hash_map[num-1]-=1
-                hash_map[num]+=1
-            elif counter[num+1] >0 and counter[num+2]>0:
-                counter[num+1] -=1
-                counter[num+2] -=1
-                hash_map[num+2] +=1
-            else:return False
-        return True
+        # counter = collections.Counter(nums)
+        # hash_map = collections.defaultdict(int)
+        # for num in nums:
+        #     if counter[num]==0:continue
+        #     counter[num]-=1
+        #     if hash_map[num-1]>0:
+        #         hash_map[num-1]-=1
+        #         hash_map[num]+=1
+        #     elif counter[num+1] >0 and counter[num+2]>0:
+        #         counter[num+1] -=1
+        #         counter[num+2] -=1
+        #         hash_map[num+2] +=1
+        #     else:return False
+        # return True
+# Time O(n)
+# Space O(n)
