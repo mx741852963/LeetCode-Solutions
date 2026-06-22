796defa0a0a3c98d21e282b19a54d17d19698146
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-22 14:29:45 +0300

    0692-top-k-frequent-words Medium Array 1 ms (68.58%) 19.6 MB (18.94%)

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,13 +1,38 @@
+class WordItem:
+    def __init__(self, freq: int, word: str):
+        self.freq = freq
+        self.word = word
+
+    def __lt__(self, other: "WordItem") -> bool:
+        if self.freq != other.freq: return self.freq < other.freq
+        return self.word > other.word
+
+    def __repr__(self) -> str:
+        return self.word
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         counter = Counter(words)
-        max_heap,res = [],[]
-        for key, value in counter.items(): heapq.heappush(max_heap, (-value, key))
-        while k :
-            k-=1
-            _ ,key = heapq.heappop(max_heap)
-            res.extend([key])
-        return res
+        heap = []
+        for key, value in counter.items():
+            if len(heap) < k:
+                heapq.heappush(heap, WordItem(value, key))
+            else:
+                heapq.heappushpop(heap, WordItem(value, key))
+        res = []
+        while heap:
+            item = heapq.heappop(heap)
+            res.append(item.word) 
+        return res[::-1]
+
 
         # Time O(n log k)
         # Space O(k)
+
+        # counter = Counter(words)
+        # max_heap,res = [],[]
+        # for key, value in counter.items(): heapq.heappush(max_heap, (-value, key))
+        # while k :
+        #     k-=1
+        #     _ ,key = heapq.heappop(max_heap)
+        #     res.extend([key])
+        # return res
