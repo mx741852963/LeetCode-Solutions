7461594fccab61a4978fb1b766c1bccdf5e55cbf
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-23 10:29:30 +0300

    3699-number-of-zigzag-arrays-i Hard Dynamic Programming 3576 ms (92.11%) 19.6 MB (78.95%)

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.py
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.py
@@ -1,10 +1,10 @@
 class Solution:
     def zigZagArrays(self, n: int, l: int, r: int) -> int: 
         mod = 10 ** 9 + 7
-        m = r - l + 1       
-        if m == 1:
-            return 1 if n == 1 else 0           
-        dp = [1] * m        
-        for _ in range(n - 1):
-            dp = [0] + [num % mod for num in itertools.accumulate(reversed(dp))][:-1]
+        m = r - l + 1  
+        dp = [1] * m      
+        if m == 1:return 1 if n == 1 else 0                 
+        for _ in range(n - 1):dp = [0] + [num % mod for num in itertools.accumulate(reversed(dp))][:-1]
         return (sum(dp) * 2) % mod
+# Time O(n*m) Spcae O(n)
+        
