761ea18d8d1c2cdb8e64f3c469be0088e2822e7d
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-23 10:19:47 +0300

    3699-number-of-zigzag-arrays-i Hard Dynamic Programming 4498 ms (89.47%) 19.5 MB (78.95%)

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.py
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.py
@@ -1,24 +1,5 @@
 class Solution:
-    def zigZagArrays(self, n: int, l: int, r: int) -> int:
-        # mod = 10 ** 9 + 7
-        # m = r - l + 1
-        # dp = [1] * m       
-        # for _ in range(n - 1):
-        #     sum_dp = list(itertools.accumulate(dp, initial=0))
-        #     x = sum_dp[-1]
-        #     dp = [(x - sum_dp[m - i]) % mod for i in range(m)]          
-        # return (sum(dp) * 2) % mod
-        # mod = 10 ** 9 + 7
-        # m = r - l + 1
-        # dp_0 = [1] * m
-        # dp_1 = [1] * m
-        # for _ in range(n - 1):
-        #     sum_0 = list(itertools.accumulate(dp_0, initial=0))
-        #     sum_1 = list(itertools.accumulate(dp_1, initial=0))
-        #     dp_0 = [num%mod for num in sum_1[:-1]]
-        #     x = sum_0[-1]
-        #     dp_1 = [(x-num) % mod for num in sum_0[1:]]
-        # return (sum(dp_1) + sum(dp_0))%mod  
+    def zigZagArrays(self, n: int, l: int, r: int) -> int: 
         mod = 10 ** 9 + 7
         m = r - l + 1       
         if m == 1:
@@ -29,8 +10,6 @@ class Solution:
             running_sum = 0
             for i in range(m - 1, 0, -1):
                 running_sum = (running_sum + dp[i]) % mod
-                new_dp[m - i] = running_sum
-                
+                new_dp[m - i] = running_sum      
             dp = new_dp
-            
         return (sum(dp) * 2) % mod
