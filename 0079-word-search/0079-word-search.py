4c53b968cde877908f384788be7edb9bc4735237
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-26 20:04:56 +0300

    0079-WORD-SEARCH Stats: Time: 3921 ms (49.95%), Space: 19.4 MB (82.57%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,15 +1,15 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m, n, w = len(board), len(board[0]), len(word)
-        if m == 1 and n == 1:
-            return board[0][0] == word
+        # if m == 1 and n == 1:
+        #     return board[0][0] == word
 
         def backtrack(pos, index):
             i, j = pos
-            if index == w:
-                return True
             if board[i][j] != word[index]:
                 return False
+            if index == w - 1:
+                return True
             char = board[i][j]
             board[i][j] = "@"
             for i_off, j_off in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
