998d424f64cdac628bc79ac184b47d7ba5f4dcac
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-06-27 18:26:00 +0300

    0040-COMBINATION-SUM-II Stats: Time: 0 ms (100%), Space: 19.6 MB (41.2%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -7,14 +7,14 @@ class Solution:
             if cur_sum == target:
                 ans.append(sol[:]) 
                 return
-            if cur_sum > target:
-                return
             for i in range(start, n):
                 if i > start and candidates[i] == candidates[i-1]:
                     continue
-                sol.append(candidates[i])
-                backtrack(i + 1, cur_sum + candidates[i])
-                sol.pop()            
+                if cur_sum + candidates[i] <= target:
+                    sol.append(candidates[i])
+                    backtrack(i + 1, cur_sum + candidates[i])
+                    sol.pop()
+                else:break           
         backtrack(0, 0)
         return ans
 # Time O(2**n)
