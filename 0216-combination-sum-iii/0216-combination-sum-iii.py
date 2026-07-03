581ba0b638e8b3a09e9210eb9f494cb8459430c3
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-03 11:36:43 +0300

    0216-COMBINATION-SUM-III Stats: Time: 0 ms (100%), Space: 19.4 MB (32.67%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,19 +1,24 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-                ans, sol = [], []
-                def backtrack(num, cur_sum):
-                    if len(sol)==k:
-                        if cur_sum == n :
-                            ans.append(sol[:]) 
-                        return
-                    if cur_sum > n :return
-                    left = num
-                    needed = k - len(sol)
-                    if left> needed:
-                        backtrack(num-1,cur_sum)
-                    if num > 0:
-                        sol.append(num)
-                        backtrack(num-1,cur_sum+ num)
-                        sol.pop()
-                backtrack(9,0)
-                return ans
+        ans, sol = [], []
+
+        def backtrack(num, cur_sum):
+            if len(sol) == k:
+                if cur_sum == n:
+                    ans.append(sol[:])
+                return
+            if cur_sum > n:
+                return
+            left = num
+            needed = k - len(sol)
+            if left > needed:
+                backtrack(num - 1, cur_sum)
+            if num > 0:
+                sol.append(num)
+                backtrack(num - 1, cur_sum + num)
+                sol.pop()
+
+        backtrack(9, 0)
+        return ans
+        # Time O(k*C(9,k))
+        # Space O(k)
