8bc3d5eab285de7c64021aa8f290e0f5067075e1
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-04 16:52:55 +0300

    0047-PERMUTATIONS-II Stats: Time: 16 ms (18.98%), Space: 19.9 MB (25.25%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -2,6 +2,7 @@ class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         ans, sol = [], []
         n = len(nums)
+        nums.sort()
 
         visited = set()
         def backtrack():
@@ -9,12 +10,15 @@ class Solution:
                 ans.append(sol[:])
                 return
             for i in range(n):
-                if i  not in visited:
-                    visited.add(i)
-                    sol.append(nums[i])
-                    backtrack()
-                    sol.pop()
-                    visited.remove(i)
+                if i   in visited:
+                    continue
+                if i > 0 and nums[i] == nums[i-1] and (i-1) not in visited:
+                    continue
+                visited.add(i)
+                sol.append(nums[i])
+                backtrack()
+                sol.pop()
+                visited.remove(i)
 
         backtrack()
 
