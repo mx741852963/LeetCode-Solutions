d9a06d1645bbe1bbdfb8b25fb14d0f414ead3117
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-06 09:59:21 +0300

    0200-NUMBER-OF-ISLANDS Stats: Time: 234 ms (83.73%), Space: 21.7 MB (48.01%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m,n = len(grid),len(grid[0])
-        def dfs(i,j):
-            if i <0 or i >=m or j <0 or j >=n or grid[i][j]!='1':
+        m, n, num_islands = len(grid), len(grid[0]), 0
+
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] != "1":
                 return
             else:
-                grid[i][j]='0'
-                # dfs(i,j+1)
-                # dfs(i+1,j)
-                # dfs(i,j-1)
-                # dfs(i-1,j)
-                for pos in [(i,j+1),(i+1,j),(i,j-1),(i-1,j)]:
-                    dfs(pos[0],pos[1])
-            
-        num_islands = 0
+                grid[i][j] = "0"
+                for pos in [(i, j + 1), (i + 1, j), (i, j - 1), (i - 1, j)]:
+                    dfs(pos[0], pos[1])
+
         for i in range(m):
             for j in range(n):
-                if grid[i][j]=='1':
-                    num_islands+=1
-                    dfs(i,j)
+                if grid[i][j] == "1":
+                    num_islands += 1
+                    dfs(i, j)
         return num_islands
-        
+# Time and Space O(n*m)
