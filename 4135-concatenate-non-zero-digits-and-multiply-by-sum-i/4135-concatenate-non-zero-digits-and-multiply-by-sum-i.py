c18a0a6781179a96dce75e581b458c6f88061c6e
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-07 21:33:31 +0300

    4135-CONCATENATE-NON-ZERO-DIGITS-AND-MULTIPLY-BY-SUM-I Stats: Time: 0 ms (100%), Space: 19.3 MB (51.25%) - LeetHub

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/4135-concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/4135-concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -2,11 +2,15 @@ class Solution:
     def sumAndMultiply(self, n: int) -> int:
         if n == 0:
             return 0
-        res, sol = 0, ""
+        res, sol, place = 0, 0, 1
         while n:
             tem = n % 10
             if tem > 0:
-                sol += "".join(str(tem))
+                sol = sol + (tem * place)
+                place *= 10
             res += tem
             n = n // 10
-        return res * int(sol[::-1])
+        return res * sol
+
+
+# Time and Space O(1)
