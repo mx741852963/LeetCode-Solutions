a125cb1eafbad243c4a0f340dee62e9c371744ee
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-07 12:27:01 +0300

    0417-PACIFIC-ATLANTIC-WATER-FLOW Stats: Time: 35 ms (38.44%), Space: 20.6 MB (87.08%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,19 +1,29 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        if not heights or not heights[0]: return []
+        if not heights or not heights[0]:
+            return []
         m, n = len(heights), len(heights[0])
         p_seen = {(i, j) for i in range(m) for j in range(n) if i == 0 or j == 0}
-        a_seen = {(i, j) for i in range(m) for j in range(n) if i == m - 1 or j == n - 1}
-        
+        a_seen = {
+            (i, j) for i in range(m) for j in range(n) if i == m - 1 or j == n - 1
+        }
+
         def bfs(seen):
             que = deque(seen)
             while que:
                 i, j = que.popleft()
-                for r, c in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-                    if 0 <= r < m and 0 <= c < n and (r, c) not in seen and heights[r][c] >= heights[i][j]:
+                for r, c in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                    if (
+                        0 <= r < m
+                        and 0 <= c < n
+                        and (r, c) not in seen
+                        and heights[r][c] >= heights[i][j]
+                    ):
                         seen.add((r, c))
                         que.append((r, c))
+
         bfs(p_seen)
         bfs(a_seen)
-        
+
         return [list(coords) for coords in p_seen.intersection(a_seen)]
+# Time and space O(m*n)
