c5162f05a35eb490d015b03bc8a8dfc3ebf315a9
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-07 22:27:36 +0300

    1706-MIN-COST-TO-CONNECT-ALL-POINTS Stats: Time: 535 ms (82.48%), Space: 83 MB (64.84%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -2,8 +2,16 @@ class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
         seen = set()
-        min_heap = [(0, 0)]
+        min_heap = []
         total_cast = 0
+        start_node = 0 
+        seen.add(start_node) 
+        xi, yi = points[start_node]
+        for j in range(n):
+            if j != start_node:  
+                xj, yj = points[j]
+                nei_dist = abs(xi - xj) + abs(yi - yj)
+                heapq.heappush(min_heap, (nei_dist, j))
         while len(seen) < n:
             dist, i = heapq.heappop(min_heap)
             if i in seen:
@@ -18,6 +26,7 @@ class Solution:
                 nei_dist = abs(xi - xj) + abs(yi - yj)
                 heapq.heappush(min_heap, (nei_dist, j))
         return total_cast
+# E =(n * (n-1))/2 
 # E = n**2
 # Time O(E log(E))
 # Space O(E)
