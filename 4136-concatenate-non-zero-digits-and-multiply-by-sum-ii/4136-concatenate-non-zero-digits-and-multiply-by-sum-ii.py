6caae2fd673e70d3d8a92dc59491e5eaf2ebdc13
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-08 16:59:05 +0300

    4136-CONCATENATE-NON-ZERO-DIGITS-AND-MULTIPLY-BY-SUM-II Stats: Time: 672 ms (6.17%), Space: 65.5 MB (6.17%) - LeetHub

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/4136-concatenate-non-zero-digits-and-multiply-by-sum-ii.py
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/4136-concatenate-non-zero-digits-and-multiply-by-sum-ii.py
@@ -1,37 +1,29 @@
 class Solution:
     def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
-        mod = 10**9 + 7
         n = len(s)
-        ans = []
-        pow10 = [1] * (n + 1)
+        mod, ans, pow10 = 10**9 + 7, [], [1] * (n + 1)
+        prefix = [[0, 0, 0] for _ in range(n + 1)]
         for i in range(1, n + 1):
             pow10[i] = (pow10[i - 1] * 10) % mod
-        pref_sum = [0] * (n + 1)
-        pref_num = [0] * (n + 1)
-        num_nums = [0] * (n + 1)
         for idx, char in enumerate(s):
             digit = int(char)
-            pref_sum[idx + 1] = pref_sum[idx] + digit
-
             if digit == 0:
-                pref_num[idx + 1] = pref_num[idx]
-                num_nums[idx + 1] = num_nums[idx]
+                prefix[idx + 1][1], prefix[idx + 1][2] = prefix[idx][1], prefix[idx][2]
             else:
-                num_nums[idx + 1] = num_nums[idx] + 1
-                pref_num[idx + 1] = (pref_num[idx] * 10 + digit) % mod
-
+                prefix[idx + 1][2], prefix[idx + 1][1] = (
+                    prefix[idx][2] + 1,
+                    (prefix[idx][1] * 10 + digit) % mod,
+                )
+            prefix[idx + 1][0] = prefix[idx][0] + digit
         for L, R in queries:
-            digit_sum = pref_sum[R + 1] - pref_sum[L]
+            digit_sum = prefix[R + 1][0] - prefix[L][0]
             if digit_sum == 0:
                 ans.append(0)
                 continue
-
-            if (k:=num_nums[R + 1] - num_nums[L])== 0:
+            if (k := prefix[R + 1][2] - prefix[L][2]) == 0:
                 ans.append(0)
                 continue
-
-            sol = (pref_num[R + 1] - pref_num[L] * pow10[k]) % mod
-
+            sol = (prefix[R + 1][1] - prefix[L][1] * pow10[k]) % mod
             ans.append((digit_sum * sol) % mod)
 
         return ans
