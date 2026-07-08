b5792a213c68ac39f3e02d2b6f24422d0a9a8520
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-08 17:37:07 +0300

    4136-CONCATENATE-NON-ZERO-DIGITS-AND-MULTIPLY-BY-SUM-II Stats: Time: 659 ms (6.17%), Space: 67 MB (6.17%) - LeetHub

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/4136-concatenate-non-zero-digits-and-multiply-by-sum-ii.py
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/4136-concatenate-non-zero-digits-and-multiply-by-sum-ii.py
@@ -1,32 +1,21 @@
 class Solution:
     def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
         n = len(s)
-        mod, ans, pow10 = 10**9 + 7, [], [1] * (n + 1)
-        prefix = [[0, 0, 0] for _ in range(n + 1)]
-        for i in range(1, n + 1):
-            pow10[i] = (pow10[i - 1] * 10) % mod
+        mod,ans,pow10 = 10**9 + 7,[],[1] * (n + 1)
+        prefix = [[0, 0, 0] for _ in range(n + 1)] 
         for idx, char in enumerate(s):
             digit = int(char)
             p_sum, p_num, p_cnt = prefix[idx]
             is_nz = int(digit != 0)
-            prefix[idx + 1] = [
-                p_sum + digit,
-                (p_num * (10**is_nz) + digit) % mod,
-                p_cnt + is_nz,
-            ]
+            prefix[idx + 1] = [p_sum + digit,(p_num * (10**is_nz) + digit) % mod,p_cnt + is_nz]
+            pow10[idx+1] = (pow10[idx] * 10) % mod
         for L, R in queries:
-            digit_sum = prefix[R + 1][0] - prefix[L][0]
-            if digit_sum == 0:
-                ans.append(0)
-                continue
-            if (k := prefix[R + 1][2] - prefix[L][2]) == 0:
+            digit_sum=(prefix[R + 1][0] - prefix[L][0])
+            if digit_sum == 0 or (k:=prefix[R + 1][2] - prefix[L][2])== 0:
                 ans.append(0)
                 continue
             sol = (prefix[R + 1][1] - prefix[L][1] * pow10[k]) % mod
             ans.append((digit_sum * sol) % mod)
-
         return ans
-
-
 # Time O(n+q)
 # Space O(n)
