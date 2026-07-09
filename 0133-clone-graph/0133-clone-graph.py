150bae42f012c439251dc80bca5dee0b5709ff5b
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-09 13:42:35 +0300

    0133-CLONE-GRAPH Stats: Time: 54 ms (28.08%), Space: 19.7 MB (53.28%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -10,22 +10,41 @@ from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node :return None
-        start = node
-        hash = {}
-        stk = [start]
-        visited = set()
-        visited.add(start)
+        
+        hash = {node:Node(val=node.val)}
+        stk = [node]
         while stk:
-            node  = stk.pop()
-            hash[node] =Node(val=node.val)
-            for nei in node.neighbors:
-                if nei not in visited:
-                    visited.add(nei)
-                    stk.append(nei)
-        for old , new in hash.items():
-            for nei in old.neighbors:
-                new_nei = hash[nei]
-                new.neighbors.append(new_nei)
-        return hash[start]
+            cur = stk.pop()
+            for neighbor in cur.neighbors:
+                if neighbor not in hash:
+                    hash[neighbor] = Node(val=neighbor.val)
+                    stk.append(neighbor)
+                hash[cur].neighbors.append(hash[neighbor])
+        return hash[node]
+
+
+
+
+
+
+
+
+
+        # start = node
+         # stk = [start]
+        # visited = set()
+        # visited.add(start)
+        # while stk:
+        #     node  = stk.pop()
+        #     hash[node] =Node(val=node.val)
+        #     for nei in node.neighbors:
+        #         if nei not in visited:
+        #             visited.add(nei)
+        #             stk.append(nei)
+        # for old , new in hash.items():
+        #     for nei in old.neighbors:
+        #         new_nei = hash[nei]
+        #         new.neighbors.append(new_nei)
+        # return hash[start]
 # Time O(V+E)
 # space O(V)
