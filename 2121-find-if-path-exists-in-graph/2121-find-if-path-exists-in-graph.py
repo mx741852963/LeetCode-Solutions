6b1c5e4f4d3847d99cdd38ef680cee498301d9b6
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-11 08:07:21 +0300

    2121-FIND-IF-PATH-EXISTS-IN-GRAPH Stats: Time: 271 ms (74.97%), Space: 96.5 MB (69.57%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
@@ -1,22 +1,36 @@
 class Solution:
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        if source == destination:return True
+    def validPath(
+        self, n: int, edges: List[List[int]], source: int, destination: int
+    ) -> bool:
+        if source == destination:
+            return True
         graph = defaultdict(list)
-        for u,v in edges:
+        for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
         seen = set()
         seen.add(source)
-        def dfs(i):
-            if i ==destination:
-                return True 
-            for nei in graph[i]:
+        stk = [source]
+        while stk:
+            node = stk.pop()
+            if node == destination:
+                return True
+            for nei in graph[node]:
                 if nei not in seen:
                     seen.add(nei)
-                    if dfs(nei):
-                        return True
-            return False
-        return dfs(source)
+                    stk.append(nei)
+        return False
 
-            
-                
+
+        # sol 1
+        # def dfs (i):
+        #     if i == destination:
+        #         return True
+        #     for nei in graph[i]:
+        #         if nei not in seen:
+        #             seen.add(nei)
+        #             if dfs(nei):
+        #                 return True
+        #     return False
+
+        # return dfs(source)
