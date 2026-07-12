dc3212ea9e04bf723be8325cd7873187c32f38b6
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-12 11:32:45 +0300

    1256-RANK-TRANSFORM-OF-AN-ARRAY Stats: Time: 31 ms (93.3%), Space: 37.5 MB (80.09%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
@@ -1,8 +1,12 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        arr2,arr3 = list(set(arr)),{}
-        n,m = len(arr2),len(arr)
-        arr2.sort()
-        for i in range(n):
-            arr3[arr2[i]] = i + 1
-        return [arr3[i] for i in arr]
+        ranks = {val: rank + 1 for rank, val in enumerate(sorted(set(arr)))}
+        return [ranks[x] for x in arr]
+        # arr2,arr3 = list(set(arr)),{}
+        # n,m = len(arr2),len(arr)
+        # arr2.sort()
+        # for i in range(n):
+        #     arr3[arr2[i]] = i + 1
+        # return [arr3[i] for i in arr]
+# Time O(n log n)
+# Space O(n)
