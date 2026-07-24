619caf1ec84a88188bfaf464391bfb57e9ea44a4
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-24 17:34:54 +0300

    1250-LONGEST-COMMON-SUBSEQUENCE Stats: Time: 357 ms (56.64%), Space: 44.4 MB (44.64%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,12 +1,25 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # Top Down DP (Memoization)
+        # # Top Down DP (Memoization)
+        # m ,n  = len(text1),len(text2)
+        # # Time and Space O(m*n)
+        # @cache
+        # def lcs(i,j):
+        #     if i == m or j == n: return 0
+        #     elif text1[i]==text2[j]:
+        #         return 1 + lcs(i+1,j+1)
+        #     else :
+        #         return max(lcs(i+1,j),lcs(i,j+1))
+        # return lcs(0,0)
+
+        # Bottom up Dp (tabulation)
         m ,n  = len(text1),len(text2)
-        @cache
-        def lcs(i,j):
-            if i == m or j == n: return 0
-            elif text1[i]==text2[j]:
-                return 1 + lcs(i+1,j+1)
-            else :
-                return max(lcs(i+1,j),lcs(i,j+1))
-        return lcs(0,0)
+        dp = [[0]*(n+1)for _ in range(m+1)]
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] +1
+                else:
+                    dp[i][j] = max( dp[i][j-1], dp[i-1][j])
+        return dp[-1][-1]
+
