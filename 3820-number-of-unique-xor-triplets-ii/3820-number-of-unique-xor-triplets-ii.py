b686f3bd6155050c9d0753b623223dc4a82f1093
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-24 12:01:00 +0300

    3820-NUMBER-OF-UNIQUE-XOR-TRIPLETS-II Stats: Time: 535 ms (100%), Space: 20 MB (11.11%) - LeetHub

--- 3820-number-of-unique-xor-triplets-ii/3820-number-of-unique-xor-triplets-ii.py
+++ 3820-number-of-unique-xor-triplets-ii/3820-number-of-unique-xor-triplets-ii.py
@@ -1,15 +1,29 @@
 class Solution:
-    def uniqueXorTriplets(self, nums: List[int]) -> int:
-        n = 1 << max(nums).bit_length()
-        one, two, three = [0] * n, [0] * n, [0] * n
-        for num in nums:
-            one[num] = 1
-            for bit in range(n):
-                if one[bit]:
-                    two[bit ^ num] = 1
-        for num in nums:
-            for bit in range(n):
-                if two[bit]:
-                    three[bit ^ num] = 1
+    def uniqueXorTriplets(self, nums: List[int]) -> int: 
+        n = 1 << max(nums). bit_length()
+        x2 = set([0])
+        x3 = set (nums)
+        while nums:
+            num = nums.pop()
+            x3 |= {num ^ x2 for x2 in x2}         
+            x2 |= {num ^ x1 for x1 in nums}  
+            if len(x3) == n: break
+ 
+        return len(x3)
+# Time O(n^2 + n*2048)
+# Space O(n + 2048)
+        
 
-        return sum(three)
+        # n = 1 << max(nums).bit_length()
+        # one, two, three = [0] * n, [0] * n, [0] * n
+        # for num in nums:
+        #     one[num] = 1
+        #     for bit in range(n):
+        #         if one[bit]:
+        #             two[bit ^ num] = 1
+        # for num in nums:
+        #     for bit in range(n):
+        #         if two[bit]:
+        #             three[bit ^ num] = 1
+
+        # return sum(three)
