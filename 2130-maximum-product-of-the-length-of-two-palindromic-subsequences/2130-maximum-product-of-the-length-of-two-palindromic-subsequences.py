97dacd4165b74e3b2b0ca4dbceaf575a6b04dc8b
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-24 21:40:28 +0300

    2130-MAXIMUM-PRODUCT-OF-THE-LENGTH-OF-TWO-PALINDROMIC-SUBSEQUENCES Stats: Time: 1187 ms (45.19%), Space: 19.6 MB (32.59%) - LeetHub

--- 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/2130-maximum-product-of-the-length-of-two-palindromic-subsequences.py
+++ 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/2130-maximum-product-of-the-length-of-two-palindromic-subsequences.py
@@ -3,13 +3,18 @@ class Solution:
         maxx = [0]
         n  =len(s)
         def backtrack(i , s1,s2):
-            if i ==n:
-                if s1 == s1[::-1 ]and s2 == s2[::-1 ]:
-                    maxx[0] = max(maxx[0],len(s1)*len(s2)) 
-                return 
+            len1, len2 = len(s1), len(s2)
+            rem = n - i
+            if (len1 + rem) * (len2 + rem) <= maxx[0]:
+                return
+            if i == n:
+                if s1 == s1[::-1] and s2 == s2[::-1]:
+                    maxx[0] = max(maxx[0], len1 * len2)
+                return
             s1.append(s[i])
             backtrack(i + 1, s1, s2)
             s1.pop() 
+            
             s2.append(s[i])
             backtrack(i + 1, s1, s2)
             s2.pop()  
