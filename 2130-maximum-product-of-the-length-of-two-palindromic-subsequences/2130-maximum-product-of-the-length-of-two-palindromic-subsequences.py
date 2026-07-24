0887cf4c26f5b9a22a1906d36173a59b52aaa47f
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-07-24 22:35:24 +0300

    2130-MAXIMUM-PRODUCT-OF-THE-LENGTH-OF-TWO-PALINDROMIC-SUBSEQUENCES Stats: Time: 1308 ms (42.03%), Space: 19.5 MB (57.97%) - LeetHub

--- 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/2130-maximum-product-of-the-length-of-two-palindromic-subsequences.py
+++ 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/2130-maximum-product-of-the-length-of-two-palindromic-subsequences.py
@@ -3,7 +3,6 @@ class Solution:
         maxx = [0]
         n = len(s)
         max_possible = (n // 2) * ((n + 1) // 2)
-
         def backtrack(i, s1, s2):
             if maxx[0] == max_possible:
                 return
@@ -15,23 +14,16 @@ class Solution:
                 if s1 == s1[::-1] and s2 == s2[::-1]:
                     maxx[0] = max(maxx[0], len1 * len2)
                 return
-            if len1 > len2:
-                s1.append(s[i])
-                backtrack(i + 1, s1, s2)
-                s1.pop()
-                if len1 > 0:
-                    s2.append(s[i])
-                    backtrack(i + 1, s1, s2)
-                    s2.pop()
-            else:
-                if len1 > 0:
-                    s2.append(s[i])
-                    backtrack(i + 1, s1, s2)
-                    s2.pop()
-                s1.append(s[i])
-                backtrack(i + 1, s1, s2)
-                s1.pop()
+            first, second = (s1, s2) if len1 > len2 else (s2, s1)
+            first.append(s[i])
             backtrack(i + 1, s1, s2)
-
+            first.pop()
+            if second is not s2 or len1 > 0:
+              second.append(s[i])
+              backtrack(i + 1, s1, s2)
+              second.pop()
+            backtrack(i + 1, s1, s2)
         backtrack(0, [], [])
         return maxx[0]
+# Time O(n^3+n)
+# Space (n)
