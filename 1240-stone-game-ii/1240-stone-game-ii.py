986f2b045b508fd2b594ebdfebc58677a80ac13b
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-08-09 15:24:10 +0300

    1240-STONE-GAME-II Stats: Time: 76 ms (83.35%), Space: 26.2 MB (49.15%) - LeetHub

--- 1240-stone-game-ii/1240-stone-game-ii.py
+++ 1240-stone-game-ii/1240-stone-game-ii.py
@@ -2,18 +2,10 @@ class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
         n = len(piles)
         suffix_sum = list(accumulate(piles[::-1]))[::-1]
-
         @cache
         def backtrack(i, M):
-            if i >= n:
-                return 0
-            if n - i <= 2 * M:
-                return suffix_sum[i]
-            return max(
-                suffix_sum[i] - backtrack(i + X, max(M, X)) for X in range(1, 2 * M + 1)
-            )
-
+            if i >= n:return 0
+            if n - i <= 2 * M:return suffix_sum[i]
+            return max(suffix_sum[i] - backtrack(i + X, max(M, X)) for X in range(1, 2 * M + 1))
         return backtrack(0, 1)
-
-
 # Time O(n*3) Space O(n*2)
