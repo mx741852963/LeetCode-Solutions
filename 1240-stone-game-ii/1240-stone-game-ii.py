68a16fbee111e6de3999199c347f4f60f5cdf852
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-08-09 15:20:56 +0300

    1240-STONE-GAME-II Stats: Time: 77 ms (83.05%), Space: 26.2 MB (49.15%) - LeetHub

--- 1240-stone-game-ii/1240-stone-game-ii.py
+++ 1240-stone-game-ii/1240-stone-game-ii.py
@@ -9,14 +9,11 @@ class Solution:
                 return 0
             if n - i <= 2 * M:
                 return suffix_sum[i]
-            max_alice_score = 0
-
-            for X in range(1, 2 * M + 1):
-                next_M = max(M, X)
-                next_i = i + X
-                opponent_score = backtrack(next_i, next_M)
-                current_alice_score = suffix_sum[i] - opponent_score
-                max_alice_score = max(max_alice_score, current_alice_score)
-            return max_alice_score
+            return max(
+                suffix_sum[i] - backtrack(i + X, max(M, X)) for X in range(1, 2 * M + 1)
+            )
 
         return backtrack(0, 1)
+
+
+# Time O(n*3) Space O(n*2)
