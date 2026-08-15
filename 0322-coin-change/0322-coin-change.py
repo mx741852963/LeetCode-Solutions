8d2e306c49acf105359462f455055ecd5686b551
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-08-15 17:42:04 +0300

    0322-COIN-CHANGE Stats: Time: 516 ms (54.96%), Space: 19.8 MB (35.02%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,18 +1,35 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        #  # Top Down with memo
+        # # Time O(coins + amount)
+        # # Space O(amount)
+        # coins.sort()
+        # @cache
+        # def min_coins(amt):
+        #     if amt == 0: return 0
+        #     minn = float("inf")
+        #     for coin in coins:
+        #         diff = amt - coin
+        #         if diff < 0:
+        #             break
+        #         minn = min(minn,1+min_coins(diff))
+        #     return minn
+        # res = min_coins(amount)
+        # if res < float("inf"):
+        #     return res
+        # else :
+        #     return -1
+
+        # Bottom Up
+        # Time O(coins + amount)
+        # Space O(amount)
         coins.sort()
-        @cache
-        def min_coins(amt):
-            if amt == 0: return 0
-            minn = float("inf")
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
             for coin in coins:
-                diff = amt - coin
-                if diff < 0:
-                    break
-                minn = min(minn,1+min_coins(diff))
-            return minn
-        res = min_coins(amount)
-        if res < float("inf"):
-            return res
-        else :
-            return -1
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i - coin])
+
+        return dp[amount] if dp[amount] != float("inf") else -1
