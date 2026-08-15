b46a7f2a3190cfd9cb4ee29f071c09943911f8ba
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-08-15 15:09:44 +0300

    0747-MIN-COST-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 19.3 MB (79.39%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,9 +1,21 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # Top Down with memo
+        # Time O(n)
+        # Space O(n)
+        # n  = len(cost)
+        # @cache
+        # def min_cost(i):
+        #     if i <  2 :
+        #         return 0
+        #     return min(cost[i-2]+min_cost(i-2),cost[i-1]+min_cost(i-1))
+        # return min_cost(n)
+
+        # Botton up  DP (Tablulation)
+        # Time O(n)
+        # Space O(n)
         n  = len(cost)
-        @cache
-        def min_cost(i):
-            if i <  2 :
-                return 0
-            return min(cost[i-2]+min_cost(i-2),cost[i-1]+min_cost(i-1))
-        return min_cost(n)
+        dp = [0] *(n+1)
+        for i in range(2,n+1):
+           dp[i]= min(cost[i-2]+dp[i-2],cost[i-1]+dp[i-1])
+        return dp[-1]
