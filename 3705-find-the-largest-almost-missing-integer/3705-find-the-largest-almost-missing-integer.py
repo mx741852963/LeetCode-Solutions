e50e3cb7cd0865225fd4413282ed08f1b2b16a57
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-08-18 09:51:37 +0300

    3705-FIND-THE-LARGEST-ALMOST-MISSING-INTEGER Stats: Time: 0 ms (100%), Space: 19.4 MB (35.66%) - LeetHub

--- 3705-find-the-largest-almost-missing-integer/3705-find-the-largest-almost-missing-integer.py
+++ 3705-find-the-largest-almost-missing-integer/3705-find-the-largest-almost-missing-integer.py
@@ -4,13 +4,11 @@ class Solution:
         n = len(nums)
         if k == n:
             return max(nums)
-        unique = []
-        for num, vreq in counter.items():
-            if vreq == 1:
-                unique.append(num)
+        unique = [num for num, freq in counter.items() if freq == 1]
         if k == 1:
             return max(unique) if unique else -1
         return max(
             nums[0] if counter[nums[0]] == 1 else -1,
             nums[-1] if counter[nums[-1]] == 1 else -1,
         )
+# Time and Space O(n)
