6434e52acaf629978370494b5fd229ac2cf87acb
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-08-26 10:06:42 +0300

    3150-SHORTEST-AND-LEXICOGRAPHICALLY-SMALLEST-BEAUTIFUL-STRING Stats: Time: 0 ms (100%), Space: 19.3 MB (30.63%) - LeetHub

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/3150-shortest-and-lexicographically-smallest-beautiful-string.py
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/3150-shortest-and-lexicographically-smallest-beautiful-string.py
@@ -2,13 +2,15 @@ class Solution:
     def shortestBeautifulSubstring(self, s: str, k: int) -> str:
         ans = ""
         h = []
-        min_len = len(s) +1 
-        for i in range(len(s)):
+        n = len(s)
+        min_len = n + 1
+        for i in range(n):
             if s[i] == "1":
                 h.append(i)
-        if len(h) < k:
+        hh = len(h)
+        if hh < k:
             return ""
-        for j in range(len(h) - k + 1):
+        for j in range(hh - k + 1):
             sub = s[h[j] : h[j + k - 1] + 1]
             if len(sub) < min_len:
                 ans = sub
@@ -16,3 +18,4 @@ class Solution:
             elif len(sub) == min_len:
                 ans = min(ans, sub)
         return ans
+#  Time and Space O(n)
