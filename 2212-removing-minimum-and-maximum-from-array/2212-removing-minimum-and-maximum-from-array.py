914796678310e124c1fbff0ed07d45338429af53
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-08-30 20:53:25 +0300

    2212-REMOVING-MINIMUM-AND-MAXIMUM-FROM-ARRAY Stats: Time: 19 ms (65.86%), Space: 31.7 MB (98.49%) - LeetHub

--- 2212-removing-minimum-and-maximum-from-array/2212-removing-minimum-and-maximum-from-array.py
+++ 2212-removing-minimum-and-maximum-from-array/2212-removing-minimum-and-maximum-from-array.py
@@ -1,25 +1,26 @@
 class Solution:
     def minimumDeletions(self, nums: List[int]) -> int:
         ans = []
-        if len(nums) == 1 :
-            ans = 1 
+        if len(nums) == 1:
+            ans = 1
             return ans
-        if len(nums) == 2 :
+        if len(nums) == 2:
             ans = 2
             return ans
-        n = len(nums)//2
+        n = len(nums) // 2
         max_num = nums.index(max(nums))
-        min_num = nums.index(min(nums))    
-        
-        min_step=len(nums[min_num:])
-        max_step=len(nums[max_num:])
-        ans.append(max(min_step,max_step))
-    
-        min_step=len(nums[:min_num+1])
-        max_step=len(nums[:max_num+1])
-        ans.append(max(min_step,max_step))
-    
-        min_step=min(len(nums[:min_num+1]),len(nums[min_num:]))
-        max_step=min(len(nums[:max_num+1]),len(nums[max_num:]))
+        min_num = nums.index(min(nums))
+
+        min_step = len(nums[min_num:])
+        max_step = len(nums[max_num:])
+        ans.append(max(min_step, max_step))
+
+        min_step = len(nums[: min_num + 1])
+        max_step = len(nums[: max_num + 1])
+        ans.append(max(min_step, max_step))
+
+        min_step = min(len(nums[: min_num + 1]), len(nums[min_num:]))
+        max_step = min(len(nums[: max_num + 1]), len(nums[max_num:]))
         ans.append(min_step + max_step)
-        return  min(ans)
+        return min(ans)
+# Time and Space O(n)
