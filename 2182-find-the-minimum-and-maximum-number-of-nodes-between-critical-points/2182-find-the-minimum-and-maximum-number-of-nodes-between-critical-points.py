98a23914148e321735d119779c1ea90554d0a163
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-08-31 12:57:27 +0300

    2182-FIND-THE-MINIMUM-AND-MAXIMUM-NUMBER-OF-NODES-BETWEEN-CRITICAL-POINTS Stats: Time: 62 ms (93.95%), Space: 63.6 MB (7.81%) - LeetHub

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -5,27 +5,40 @@
 #         self.next = next
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
-        hash = defaultdict(int)
-        hash[1] = head.val
-        enumrate = 2
-        while head:
-            head = head.next
-            if head:
-                hash[enumrate] = head.val
-                enumrate += 1
-        if len(hash) <= 2:
-            return [-1, -1]
-        n = len(hash)
-        cp = []
+        # hash = defaultdict(int)
+        # hash[1] = head.val
+        # enumrate = 2
+        # while head:
+        #     head = head.next
+        #     if head:
+        #         hash[enumrate] = head.val
+        #         enumrate += 1
+        # if len(hash) <= 2:
+        #     return [-1, -1]
+        # n = len(hash)
+        # cp = []
 
-        for idx in range(2, n):
-            if hash[idx - 1] > hash[idx] < hash[idx + 1]:
-                cp.append(idx)
-            elif hash[idx - 1] < hash[idx] > hash[idx + 1]:
-                cp.append(idx)
-        if len(cp) < 2:
-            return [-1,-1]
-        return [ 
-            min([((cp[x + 1]) - cp[x]) for x in range(len(cp) - 1)]),
-            cp[-1] - cp[0],
-        ] 
+        # for idx in range(2, n):
+        #     if hash[idx - 1] > hash[idx] < hash[idx + 1]:
+        #         cp.append(idx)
+        #     elif hash[idx - 1] < hash[idx] > hash[idx + 1]:
+        #         cp.append(idx)
+        # if len(cp) < 2:
+        #     return [-1,-1]
+        # return [ 
+        #     min([((cp[x + 1]) - cp[x]) for x in range(len(cp) - 1)]),
+        #     cp[-1] - cp[0],
+        # ] 
+        min_dist, first, last = float('inf'), -1, -1
+        prev, cur, nxt = head, head.next, head.next.next
+        i = 0
+        while nxt:
+            if prev.val > cur.val< nxt.val or prev.val < cur.val> nxt.val:
+                if first == -1:
+                    first = i
+                if first != -1 and last != -1:
+                    min_dist = min(min_dist, i-last)
+                last = i
+            i += 1
+            prev, cur, nxt = cur, nxt, nxt.next
+        return [min_dist, last-first] if last != first else [-1,-1]
