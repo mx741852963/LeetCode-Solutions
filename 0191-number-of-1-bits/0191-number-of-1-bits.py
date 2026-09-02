7ba1651d6d13751b5320de7feda3a0a255eb91fb
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-09-02 14:16:40 +0300

    0191-NUMBER-OF-1-BITS Stats: Time: 0 ms (100%), Space: 19.3 MB (56.36%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,3 +1,12 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-       return Counter(bin(n)[2:])["1"]
+    #    return Counter(bin(n)[2:])["1"]
+
+# second  sol
+        ans = 0
+        while n :
+            ans+=1
+            n = n&(n-1)
+        return ans
+# Time O(bits)
+# Space O(1)
