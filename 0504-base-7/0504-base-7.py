5b2ea912d9fe6884bfcd18f9b1a9c659d6db2b77
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-09-02 14:46:20 +0300

    0504-BASE-7 Stats: Time: 0 ms (100%), Space: 19.3 MB (70.24%) - LeetHub

--- 0504-base-7/0504-base-7.py
+++ 0504-base-7/0504-base-7.py
@@ -1,13 +1,13 @@
 class Solution:
     def convertToBase7(self, num: int) -> str:
-        if num == 0 : return '0'
-        org_num = num
+        if num == 0:
+            return "0"
+        sign = "-" if num < 0 else ""
         num = abs(num)
-        res = []
+        res = ""
         while num:
-            rem = num % 7
-            res.append(str(rem))
-            num = num //7
-        if org_num < 0 :
-            res.append('-')
-        return ''.join(res[::-1])
+            res = str(num % 7) + res
+            num = num // 7
+
+        return sign + res
+    # Time and Space O(n)
