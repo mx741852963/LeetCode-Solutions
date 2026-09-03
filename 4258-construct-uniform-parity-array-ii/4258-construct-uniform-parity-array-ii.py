985368dfc5c6fe1119f31eefec0c784e6b9de49c
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-09-03 22:05:59 +0300

    4258-CONSTRUCT-UNIFORM-PARITY-ARRAY-II Stats: Time: 14 ms (86.59%), Space: 35.4 MB (51.83%) - LeetHub

--- 4258-construct-uniform-parity-array-ii/4258-construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/4258-construct-uniform-parity-array-ii.py
@@ -1,3 +1,4 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-      return True if min(nums1)&1 else False if 1 in [x & 1 for x in nums1 ] else True      
+      return True if min(nums1)&1  else not any(x & 1 for x in nums1)
+# Time O(n) Space O(1)
