58da6af2c3b93584e39545655caaa02004e7c150
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-09-12 12:08:44 +0300

    3799-UNIQUE-3-DIGIT-EVEN-NUMBERS Stats: Time: 23 ms (44.3%), Space: 20.3 MB (10.31%) - LeetHub

--- 3799-unique-3-digit-even-numbers/3799-unique-3-digit-even-numbers.py
+++ 3799-unique-3-digit-even-numbers/3799-unique-3-digit-even-numbers.py
@@ -1,23 +1,29 @@
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
-        self.ans, sol, n = 0, [], len(digits)
+        sol, n = [], len(digits)
         visited = set()
         digits.sort()
-        def backtrack():
-            if len(sol) == 3:
-                if sol[0] != 0 and not sol[2] & 1:
-                    self.ans += 1
-                return
+
+        def backtrack(count):
+            curr_len = len(sol)
+            if curr_len == 3:
+                return count + 1
             for i in range(n):
                 if i in visited:
                     continue
                 if i > 0 and digits[i] == digits[i - 1] and (i - 1) not in visited:
                     continue
+                if curr_len == 0 and digits[i] == 0:
+                    continue
+                if curr_len == 2 and (digits[i] & 1):
+                    continue
                 visited.add(i)
                 sol.append(digits[i])
-                backtrack()
+                count = backtrack(count)
                 sol.pop()
                 visited.remove(i)
+            return count
 
-        backtrack()
-        return self.ans
+        return backtrack(0)
+# Time O(n**3) : n * n - 1 * n -2
+# Space O(n) : sort func
