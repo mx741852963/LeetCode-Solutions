5ab0d16938bfdee41998ae76227e0d23184b374e
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-09-16 10:17:52 +0300

    1725-NUMBER-OF-SETS-OF-K-NON-OVERLAPPING-LINE-SEGMENTS Stats: Time: 0 ms (100%), Space: 19.5 MB (68.1%) - LeetHub

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/1725-number-of-sets-of-k-non-overlapping-line-segments.py
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/1725-number-of-sets-of-k-non-overlapping-line-segments.py
@@ -1,19 +1,22 @@
 class Solution:
     def numberOfSets(self, n: int, k: int) -> int:
-        # return comb(n+k-1,k*2)%(10**9+7)
-        mod = 10**9 + 7
+        return comb(n+k-1,k*2)%(10**9+7)
+        # Time O(min(n+k-1,k*2))
+        # Space O(1)
+        # mod = 10**9 + 7
+        # @cache
+        # def dp(i, seg, first):
+        #     if seg == 0:
+        #         return 1
+        #     if i == n:
+        #         return 0
+        #     res = dp(i+1, seg, first)
+        #     if first:
+        #         res += dp(i + 1, seg, False)
+        #     else:
+        #         res += dp(i, seg - 1, True)
+        #     return res % mod
 
-        @cache
-        def dp(i, seg, first):
-            if seg == 0:
-                return 1
-            if i == n:
-                return 0
-            res = dp(i+1, seg, first)
-            if first:
-                res += dp(i + 1, seg, False)
-            else:
-                res += dp(i, seg - 1, True)
-            return res % mod
+        # return dp(0, k, True)
+        # Time and Space O(nk)
 
-        return dp(0, k, True)
