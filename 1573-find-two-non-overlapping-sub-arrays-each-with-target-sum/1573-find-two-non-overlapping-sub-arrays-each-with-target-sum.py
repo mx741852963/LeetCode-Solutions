93244186fddd0299d328f582dd813107666d83f0
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-09-17 14:26:10 +0300

    1573-FIND-TWO-NON-OVERLAPPING-SUB-ARRAYS-EACH-WITH-TARGET-SUM Stats: Time: 129 ms (62.57%), Space: 31.2 MB (57.54%) - LeetHub

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/1573-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/1573-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -4,15 +4,19 @@ class Solution:
         l = 0
         dp = [inf] * n
         cur_sum = 0
-        ans =best_len= inf
+        ans = best_len = inf
         for r in range(n):
             cur_sum += arr[r]
             while cur_sum > target:
                 cur_sum -= arr[l]
                 l += 1
             if cur_sum == target:
-                ans = min(ans, r - l + 1 + (dp[l - 1]if l > 0 and dp[l - 1] != inf else inf ))
+                ans = min(
+                    ans, r - l + 1 + (dp[l - 1] if l > 0 and dp[l - 1] != inf else inf)
+                )
                 best_len = min(best_len, r - l + 1)
             dp[r] = min(dp[r - 1] if r > 0 else inf, best_len)
         return ans if ans != inf else -1
+
+
 # Time and Space O(n)
