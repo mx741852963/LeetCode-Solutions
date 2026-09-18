2cd9fd547a4af29b165b714829747e8211b8d8fb
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-09-18 13:42:14 +0300

    1644-MAXIMUM-NUMBER-OF-NON-OVERLAPPING-SUBSTRINGS Stats: Time: 240 ms (42.03%), Space: 26.7 MB (10.18%) - LeetHub

--- 1644-maximum-number-of-non-overlapping-substrings/1644-maximum-number-of-non-overlapping-substrings.py
+++ 1644-maximum-number-of-non-overlapping-substrings/1644-maximum-number-of-non-overlapping-substrings.py
@@ -2,29 +2,29 @@ class Solution:
     def maxNumOfSubstrings(self, s: str) -> list[str]:
         hash = defaultdict(list)
         ans = []
-        for i in range(len(s)) :
+        for i in range(len(s)):
             hash[s[i]].append(i)
         for char in hash:
             l = hash[char][0]
-            r  = hash[char][-1]
+            r = hash[char][-1]
             is_valid = True
-            i= l
-            while i <=r:
+            i = l
+            while i <= r:
                 ch = s[i]
-                
-                if hash[ch][0] < l :
+                if hash[ch][0] < l:
                     is_valid = False
                     break
-                r = max(r,hash[ch][-1])
-                i+=1
-            if is_valid : ans.append([l,r])
-        ans.sort(key=lambda x :x[-1])
+                r = max(r, hash[ch][-1])
+                i += 1
+            if is_valid:
+                ans.append([l, r])
+        print(hash,ans)
+        ans.sort(key=lambda x: x[-1])
         result = []
         last_r = -1
         for l, r in ans:
             if l > last_r:
-                result.append(s[l:r+1])
+                result.append(s[l : r + 1])
                 last_r = r
         return result
-
-                
+    # Space and Time O(n)
