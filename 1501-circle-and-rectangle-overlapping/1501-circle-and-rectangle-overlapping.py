28c00368594658543e4f72d78e47697bd61c534f
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-09-19 20:58:35 +0300

    1501-CIRCLE-AND-RECTANGLE-OVERLAPPING Stats: Time: 0 ms (100%), Space: 19.2 MB (88%) - LeetHub

--- 1501-circle-and-rectangle-overlapping/1501-circle-and-rectangle-overlapping.py
+++ 1501-circle-and-rectangle-overlapping/1501-circle-and-rectangle-overlapping.py
@@ -9,20 +9,23 @@ class Solution:
         x2: int,
         y2: int,
     ) -> bool:
-        closest_x = 0
-        if xCenter < x1:
-            closest_x = x1
-        elif xCenter > x2:
-            closest_x = x2
-        else:
-            closest_x = xCenter
-        closest_y = 0
-        if yCenter < y1:
-            closest_y = y1
-        elif yCenter > y2:
-            closest_y = y2
-        else:
-            closest_y = yCenter
+        # closest_x = 0
+        # if xCenter < x1:
+        #     closest_x = x1
+        # elif xCenter > x2:
+        #     closest_x = x2
+        # else:
+        #     closest_x = xCenter
+        # closest_y = 0
+        # if yCenter < y1:
+        #     closest_y = y1
+        # elif yCenter > y2:
+        #     closest_y = y2
+        # else:
+        #     closest_y = yCenter
+        closest_x = max(x1, min(xCenter, x2))
+        closest_y = max(y1, min(yCenter, y2))
         dx = xCenter - closest_x
         dy = yCenter - closest_y
         return (dx**2 + dy**2) <= (radius**2)
+        # Time and Spcae O(1)
