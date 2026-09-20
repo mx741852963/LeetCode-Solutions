abcd4481909431189d78de4b5658a5f834d780cd
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-09-20 13:40:52 +0300

    3811-REVERSE-DEGREE-OF-A-STRING Stats: Time: 4 ms (87.13%), Space: 19.3 MB (55.57%) - LeetHub

--- 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.py
@@ -1,3 +1,5 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
-        return sum((26 - (ord(char) - 97)) * i for i, char in enumerate(s, 1))
+        return sum((123- ord(char)) * i for i, char in enumerate(s, 1))
+        # Time O(n)
+        # Space O(1)
