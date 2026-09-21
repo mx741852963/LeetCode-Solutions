6e27d94625bf4d77d27443dfa8e6d7fc520659d7
Ahmad <145445286+mx741852963@users.noreply.github.com>
2026-09-21 11:01:43 +0300

    3831-FIND-X-VALUE-OF-ARRAY-I Stats: Time: 303 ms (86.84%), Space: 34.1 MB (85.53%) - LeetHub

--- 3831-find-x-value-of-array-i/3831-find-x-value-of-array-i.py
+++ 3831-find-x-value-of-array-i/3831-find-x-value-of-array-i.py
@@ -1,11 +1,18 @@
 class Solution:
     def resultArray(self, nums: List[int], k: int) -> List[int]:
-        n = len(nums)
-        dp = [[0] * k for _ in range(n)]
-        for i in range(n):
-            dp[i][nums[i] % k] += 1
-            if i == 0:
-                continue
+        ans = [0] * k
+        dp = [0] * k
+        for num in nums:
+            n_dp = [0] * k
+            n_dp[num % k] = 1
             for r in range(k):
-                dp[i][(r * nums[i]) % k] += dp[i - 1][r]
-        return [sum(dp[i][r] for i in range(n)) for r in range(k)]
+                if dp[r] == 0:
+                    continue
+                n_dp[(r * num) % k] += dp[r]
+            dp = n_dp
+            for r in range(k):
+                ans[r] += dp[r]
+        return ans
+
+
+# Time and Space O(n*k)
